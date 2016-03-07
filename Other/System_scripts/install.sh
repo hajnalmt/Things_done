c4eac25fcbd632109934f3164e012003f6657cd4
Hajnal Máté <hajnalmt@gmail.com>
2016-03-07 23:49:21 +0100

    Install script: Vbox+sublime added

--- Other/System_scripts/install.sh
+++ Other/System_scripts/install.sh
@@ -8,7 +8,8 @@ INSTALLED_PACKS=$(dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2)
 # Wanted Packages stored in an array, just extend it by adding packages to it
 WANTED_PACKS=(
 	codeblocks dos2unix filezilla firefox git giggle gimp gparted geany \
-	ktorrent libappindicator1 libindicator7 libxss1 lm-sensors lp-solve mc mypaint p7zip p7zip-full putty \
+	ktorrent libappindicator1 libindicator7 libxss1 lm-sensors lp-solve mc \
+	mypaint p7zip p7zip-full putty \
 	qtcreator vifm vim vlc wpagui wpasupplicant
 )
 
@@ -31,26 +32,73 @@ fi
 ARCH=$(uname -m)
 
 # Change to your home directory
-cd /~
+cd ~
 
 # Download packages from web and then install them
 if [ $ARCH == "x86_64" ]; then
-	if [ -d Programs_64]; then
+	if [ ! -d Programs_64 ]; then
 		mkdir Programs_64
 	fi
 	cd ~/Programs_64
-	# Download packages from web.  
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -i *.deb
+	# If the package isn't installed install it, if it isn't downloaded download it also.
 	
-elif [ $ARCH = "i386" ]; then	echo "32 bites architektúra" 
-	if [ -d Programs_32]; then
+	#CHROME#  
+	if [ ! -e "/usr/bin/google-chrome-stable" ]; then
+		if [ ! -e "~/Programs_64/google-chrome-stable_current_amd64.deb" ]; then 
+			wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+		fi
+		sudo dpkg -i google-chrome-stable_current_amd64.deb
+	fi
+
+	#SUBLIME#
+        if [ ! -e "/usr/bin/subl" ]; then
+                if [ ! -e "~/Programs_64/sublime-text_build-3103_amd64.deb" ]; then
+                        wget https://download.sublimetext.com/sublime-text_build-3103_amd64.deb
+                fi
+                sudo dpkg -i sublime-text_build-3103_amd64.deb
+        fi
+
+	#VIRTUALBOX#
+        if [ ! -e "/usr/bin/virtualbox" ]; then
+                if [ ! -e "~/Programs_64/virtualbox-5.0_5.0.16-105871~Ubuntu~trusty_amd64.deb" ]; then
+                        wget http://download.virtualbox.org/virtualbox/5.0.16/virtualbox-5.0_5.0.16-105871~Ubuntu~trusty_amd64.deb
+                fi
+                sudo dpkg -i virtualbox-5.0_5.0.16-105871~Ubuntu~trusty_i386.deb
+        fi
+
+
+elif [ $ARCH = "i386" ]; then 
+	if [ ! -d Programs_32 ]; then
                 mkdir Programs_32
         fi
         cd ~/Programs_32
-        # Download packages from web.  
-        wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-	sudo dpkg -i *.deb
+        # If the package isn't installed -> install it, if it isn't downloaded -> download it also.
+
+        #CHROME#  
+        if [ ! -e "/usr/bin/google-chrome-stable" ]; then
+                if [ ! -e "~/Programs_32/google-chrome-stable_current_i386.deb" ]; then
+                	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+                fi
+                sudo dpkg -i google-chrome-stable_current_i386.deb
+        fi
+	
+	#SUBLIME#
+        if [ ! -e "/usr/bin/subl" ]; then
+                if [ ! -e "~/Programs_32/sublime-text_build-3103_i386.deb" ]; then
+                	wget https://download.sublimetext.com/sublime-text_build-3103_i386.deb
+                fi
+                sudo dpkg -i sublime-text_build-3103_i386.deb
+        fi
+
+	#VIRTUALBOX#
+        if [ ! -e "/usr/bin/virtualbox" ]; then
+                if [ ! -e "~/Programs_32/virtualbox-5.0_5.0.16-105871~Ubuntu~trusty_i386.deb" ]; then
+                        wget http://download.virtualbox.org/virtualbox/5.0.16/virtualbox-5.0_5.0.16-105871~Ubuntu~trusty_i386.deb
+                fi
+                sudo dpkg -i virtualbox-5.0_5.0.16-105871~Ubuntu~trusty_i386.deb
+        fi
+
+
 else exit
 fi
  
