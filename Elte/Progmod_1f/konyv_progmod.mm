e1a12ecf54826c41f77be557cfa8b980929aa6c2
Hajnal Máté <hajnalmt@gmail.com>
2016-03-09 14:54:13 +0100

    Progmod: Start phase of the mindmap for the book added

---
+++ Elte/Progmod_1f/konyv_progmod.mm
@@ -0,0 +1,36 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1457530905013" ID="ID_1385523408" MODIFIED="1457531419623" TEXT="Bevezet&#xe9;s a programoz&#xe1;shoz">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      T&#225;rgy1: Programoz&#225;s m&#243;dszertani alapjai 1.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      A k&#246;nyvet &#237;rta: F&#243;thi &#193;kos
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1457531014862" ID="ID_1305375914" MODIFIED="1457531061155" POSITION="right" TEXT="1. Alapfogalmak">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1457531063935" ID="ID_1965263102" MODIFIED="1457531063935" TEXT=""/>
+</node>
+<node CREATED="1457531071847" ID="ID_409542244" MODIFIED="1457531091539" POSITION="right" TEXT="2. A programoz&#xe1;s alapfogalmai">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1457531102895" ID="ID_1702385452" MODIFIED="1457531119089" POSITION="right" TEXT="3. Specifik&#xe1;ci&#xf3;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1457531122294" ID="ID_1888957416" MODIFIED="1457531134727" POSITION="right" TEXT="4. Kiterjeszt&#xe9;sek">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</map>
