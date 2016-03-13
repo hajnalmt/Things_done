19042f3ef7128d8dd03622850b8c6a08c22bee84
Hajnal Máté <hajnalmt@gmail.com>
2016-03-13 21:48:42 +0100

    Dokszerk: Started LaTex mindmap

---
+++ BME/Dokszerk_1f/LaTex.mm
@@ -0,0 +1,168 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1457897031530" ID="ID_1295492209" MODIFIED="1457897094561" TEXT="LaTex">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A BME szabadonv&#225;lasztott dokumentumszerkeszt&#233;s nevezet&#369; t&#225;rgy&#225;hoz k&#233;sz&#252;lt mindmap.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1457897196830" ID="ID_1169635182" MODIFIED="1457897245741" POSITION="right" TEXT="Hogyan n&#xe9;z ki egy ig&#xe9;nyes domkumentum?">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      N&#233;h&#225;ny sz&#243;ban az alapvet&#337; tipogr&#225;fiai szab&#225;lyokr&#243;l.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1457897300022" FOLDED="true" ID="ID_373378360" MODIFIED="1457897672649" TEXT="Megfelel&#x151; sz&#xf6;vegszerkeszt&#x151;!">
+<node CREATED="1457897356874" ID="ID_1939913387" MODIFIED="1457897370598" TEXT="Sublime Linuxon"/>
+<node CREATED="1457897371886" ID="ID_1097468464" MODIFIED="1457897383796" TEXT="Notepad++ Windowson"/>
+<node CREATED="1457897393887" ID="ID_24452270" MODIFIED="1457897403067" TEXT="A LaTex scriptnyelv">
+<node CREATED="1457897437160" ID="ID_942999422" MODIFIED="1457897460124" TEXT="A helyes&#xed;r&#xe1;s ellen&#x151;rz&#xe9;s is megoldhat&#xf3;"/>
+<node CREATED="1457897468967" ID="ID_326633068" MODIFIED="1457897490676" TEXT="Irodalomhivatkoz&#xe1;s egyszer&#x171;"/>
+</node>
+</node>
+<node CREATED="1457897518054" FOLDED="true" ID="ID_1978457836" MODIFIED="1457898133984" TEXT="Megfelel&#x151; oprendszer &#xe9;s nyelv!">
+<node CREATED="1457897585615" ID="ID_1393655193" MODIFIED="1457897618035" TEXT="Idegen nyelv&#x171;vel k&#xfc;lf&#xf6;ld&#xf6;n is &#xe9;rv&#xe9;nyes&#xfc;lhetsz"/>
+<node CREATED="1457897639054" ID="ID_596278543" MODIFIED="1457897668579" TEXT="Anyanyelv&#xe9;n tudja legjobban kifejezni az ember mag&#xe1;t"/>
+<node CREATED="1457897710070" ID="ID_220846898" MODIFIED="1457897732669" TEXT="Szakzsargon &#xe9;s szakkifejez&#xe9;s megfelel&#x151; haszn&#xe1;lata"/>
+<node CREATED="1457897760357" ID="ID_240269204" MODIFIED="1457897772076" TEXT="Mindig olvassa &#xe1;t valaki a munk&#xe1;nkat!"/>
+</node>
+<node CREATED="1457897787062" FOLDED="true" ID="ID_1027688179" MODIFIED="1457898740915" TEXT="Magyar nyelv saj&#xe1;toss&#xe1;gai">
+<node CREATED="1457897957878" ID="ID_151477660" MODIFIED="1457897960020" TEXT="A mondatok nagy bet&#x171;vel kezd&#x151;dnek &#xe9;s ponttal, felki&#xe1;lt&#xf3;jellel, vagy k&#xe9;rd&#x151;jellel z&#xe1;r&#xf3;dnak"/>
+<node CREATED="1457897967334" ID="ID_1439812383" MODIFIED="1457897968533" TEXT="Mondaton bel&#xfc;l vessz&#x151;t, pontosvessz&#x151;t, kett&#x151;spontot, gondolatjelet &#xe9;s elv&#xe1;laszt&#xe1;s eset&#xe9;n k&#xf6;t&#x151;jelet haszn&#xe1;lunk"/>
+<node CREATED="1457897980584" ID="ID_386646642" MODIFIED="1457897987648" TEXT="Magyarban a felsorol&#xe1;s eset&#xe9;n a felsorolt tagok k&#xf6;z&#xe9; vessz&#x151; ker&#xfc;l ">
+<node CREATED="1457897988823" ID="ID_1122861893" MODIFIED="1457897989841" TEXT="(kiv&#xe9;ve az &#x201e;&#xe9;s&#x201d; sz&#xf3; el&#xe9;)"/>
+</node>
+<node CREATED="1457898030766" FOLDED="true" ID="ID_1208013737" MODIFIED="1457898315433" TEXT="Angolban m&#xe1;s szab&#xe1;lyok vannak &#xe9;rv&#xe9;nyben">
+<node CREATED="1457898033831" ID="ID_1556167837" MODIFIED="1457898042390" TEXT="k&#xe9;t tag k&#xf6;z&#xe9; csak egy &#x201e;and&#x201d; sz&#xf3;cska ker&#xfc;l">
+<node CREATED="1457898052345" ID="ID_1617747271" MODIFIED="1457898300523" TEXT="(Bee and Boe)">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898061023" ID="ID_1927946220" MODIFIED="1457898062957" TEXT="h&#xe1;romtagb&#xf3;l az els&#x151; kett&#x151;t vessz&#x151;vel v&#xe1;lasztjuk el">
+<node CREATED="1457898074543" ID="ID_1758697786" MODIFIED="1457898301956" TEXT="(Lock, Stock and Two Smoking Barrels)">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898085894" ID="ID_199815874" MODIFIED="1457898088205" TEXT="n&#xe9;gy, illetve t&#xf6;bb tag eset&#xe9;n mindenhova kell vessz&#x151;">
+<node CREATED="1457898098047" ID="ID_846836483" MODIFIED="1457898303046" TEXT="(vodka, gin, whiskey, and tequila)">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1457898160591" FOLDED="true" ID="ID_1062383949" MODIFIED="1457898314345" TEXT="A nyit&#xf3; z&#xe1;r&#xf3;jel &#xe9;s z&#xe1;r&#xf3; z&#xe1;r&#xf3;jel k&#xf6;z&#xe9; ker&#xfc;l&#x151; szavakat szorosan kell bez&#xe1;rni, sz&#xf3;k&#xf6;z n&#xe9;lk&#xfc;l">
+<node CREATED="1457898174522" ID="ID_1640000793" MODIFIED="1457898188380" TEXT="magyarban">
+<node CREATED="1457898191255" ID="ID_732445473" MODIFIED="1457898303682" TEXT="&#x201e;99&#x201d;">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898227039" ID="ID_773457958" MODIFIED="1457898228364" TEXT="angol">
+<node CREATED="1457898253687" ID="ID_588548608" MODIFIED="1457898304217" TEXT="&#x2018;British English&#x2019;">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898230214" ID="ID_130962119" MODIFIED="1457898233747" TEXT="USA-ban">
+<node CREATED="1457898275270" ID="ID_1170253565" MODIFIED="1457898304697" TEXT="&#x201c;American English&#x201d;">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898235494" ID="ID_176855505" MODIFIED="1457898241011" TEXT="n&#xe9;met nyelv">
+<node CREATED="1457898282519" ID="ID_556929386" MODIFIED="1457898305074" TEXT="&#x201e;Deutsch&#x201c;">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898243383" ID="ID_1676383389" MODIFIED="1457898250575" TEXT="francia nyelv">
+<node CREATED="1457898289237" ID="ID_1345312403" MODIFIED="1457898305521" TEXT="&#xab; Fran&#xe7;ais &#xbb;">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1457898322206" ID="ID_1393712833" MODIFIED="1457898344244" TEXT="Magyarban az id&#xe9;z&#x151;jelek egym&#xe1;sba &#xe1;gyaz&#xe1;s&#xe1;n&#xe1;l a &#x201e;norm&#xe1;l&#x201d; id&#xe9;z&#x151;jelen bel&#xfc;l &#x201e;a &#xbb;hegy&#xe9;vel befel&#xe9; fordul&#xf3; jelp&#xe1;r&#xab; (l&#xfa;dl&#xe1;b) megold&#xe1;st haszn&#xe1;lhatjuk&#x201d;"/>
+<node CREATED="1457898360894" ID="ID_1103475421" MODIFIED="1457898361890" TEXT="&quot; jel (az angol h&#xfc;velyk &#x2013; inch) t&#xe1;vols&#xe1;gm&#xe9;rt&#xe9;k"/>
+<node CREATED="1457898369494" ID="ID_19135146" MODIFIED="1457898370694" TEXT="&#x2032; jel a percet, dupl&#xe1;n &#x2032;&#x2032; a m&#xe1;sodpercet jel&#xf6;li (f&#x151;leg f&#xf6;ldrajzi koordin&#xe1;t&#xe1;kban), &#xed;gy a 20 &#x25e6; 11&#x2032;12&#x2032;&#x2032; egy&#xe9;rtelm&#x171;en nem id&#xe9;z&#x151;jelek"/>
+<node CREATED="1457898417382" ID="ID_1081348214" MODIFIED="1457898418670" TEXT="ha egyszer&#x171; sz&#xe1;mr&#xf3;l van sz&#xf3;, c&#xe9;lszer&#x171; ki&#xed;rni bet&#x171;vel, hiszen &#xfa;gy jobban mutatnak">
+<node CREATED="1457898444535" ID="ID_1062689814" MODIFIED="1457898459552" TEXT="K&#xe9;t v&#xe9;lem&#xe9;nyen voltunk, pedig sz&#xe1;zszor elmondtam, hogy nincs igaza">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898432934" ID="ID_1386063197" MODIFIED="1457898439351" TEXT="Magyarban tizedesvessz&#x151; van, angolban tizedespont &#x2013; Ne keverj&#xfc;k! &#x3c0;=3,14 vagy &#x3c0;= 3.14">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1457898473423" FOLDED="true" ID="ID_1704055785" MODIFIED="1457898738592" TEXT="A helyi&#xe9;rt&#xe9;kek megtal&#xe1;l&#xe1;s&#xe1;t seg&#xed;thetj&#xfc;k">
+<node CREATED="1457898541038" ID="ID_1575980416" MODIFIED="1457898552308" TEXT="magyarban sz&#xf3;k&#xf6;z besz&#xfa;r&#xe1;s">
+<node CREATED="1457898499014" ID="ID_1925317037" MODIFIED="1457898586561" TEXT="Vigy&#xe1;zzunk, hogy a sz&#xf3;k&#xf6;z&#xf6;kn&#xe9;l ne t&#xf6;rhessen a sor">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898589246" ID="ID_1879017419" MODIFIED="1457898602420" TEXT="angolban vessz&#x151;"/>
+<node CREATED="1457898613183" ID="ID_1193099837" MODIFIED="1457898624116" TEXT="Ha a sz&#xe1;m nem nagyobb 9999-n&#xe9;l akkor nem szok&#xe1;s"/>
+</node>
+<node CREATED="1457898632231" FOLDED="true" ID="ID_1980101976" MODIFIED="1457898737634" TEXT="P&#xe9;nznemek">
+<node CREATED="1457898638119" ID="ID_1388301762" MODIFIED="1457898652645" TEXT="Magyarban &#xf6;sszeg ut&#xe1;n nem t&#xf6;rhet&#x151; sz&#xf3;k&#xf6;z">
+<node CREATED="1457898653854" ID="ID_554371772" MODIFIED="1457898660640" TEXT="100 Ft">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457898669023" ID="ID_1128150920" MODIFIED="1457898675421" TEXT="angolban a p&#xe9;nznem sz&#xf3;k&#xf6;z n&#xe9;lk&#xfc;l el&#x151;zi meg az &#xf6;sszeget">
+<node CREATED="1457898683607" ID="ID_1687393773" MODIFIED="1457898685332" TEXT="$100"/>
+</node>
+<node CREATED="1457898704094" ID="ID_1689456152" MODIFIED="1457898705342" TEXT="Magyarban a p&#xe9;nznemek r&#xf6;vid&#xed;tve mindig nagy bet&#x171;vel kezd&#x151;dnek, ki&#xed;rva viszont kis bet&#x171;vel kell &#x151;ket kezdeni"/>
+<node CREATED="1457898711895" ID="ID_563268958" MODIFIED="1457898713312" TEXT="A milli&#xf3;s t&#xe9;teleket nagy &#x201e;M&#x201d;-mel, a milli&#xe1;rdos t&#xe9;teleket &#x201e;Mrd&#x201d;- vel jel&#xf6;lj&#xfc;k"/>
+</node>
+</node>
+<node CREATED="1457898743999" FOLDED="true" ID="ID_1737839123" MODIFIED="1457901440369" TEXT="M&#xe9;tr&#xe9;kegys&#xe9;gek">
+<node CREATED="1457899623119" ID="ID_708650703" MODIFIED="1457899624806" TEXT="sz&#xe1;m ut&#xe1;n kell &#xed;rni"/>
+<node CREATED="1457899629935" ID="ID_83635399" MODIFIED="1457899640582" TEXT="nem t&#xf6;rhet&#x151; sz&#xf3;k&#xf6;z a m&#xe9;rt&#xe9;kegys&#xe9;g &#xe9;s a sz&#xe1;m k&#xf6;z&#xe9;">
+<node CREATED="1457899641623" ID="ID_1354043183" MODIFIED="1457899644259" TEXT="Angolban nem">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457899650615" ID="ID_1780783063" MODIFIED="1457899660306" TEXT="&#xc1;ltal&#xe1;ban r&#xf6;vid&#xed;t&#xe9;sek de nem tesz&#xfc;nk ut&#xe1;nuk pontot"/>
+<node CREATED="1457899798911" ID="ID_989533967" MODIFIED="1457899805604" TEXT="Ki&#xed;rva mindig kisbet&#x171;vel"/>
+<node CREATED="1457899813662" ID="ID_1591205650" MODIFIED="1457899841989" TEXT="SI m&#xf3;dos&#xed;t&#xf3;k mindig a m&#xe9;rt&#xe9;kegys&#xe9;g el&#xe9;"/>
+</node>
+<node CREATED="1457901442352" FOLDED="true" ID="ID_1870748221" MODIFIED="1457901617250" TEXT="Id&#x151;pontok, d&#xe1;tumok">
+<node CREATED="1457901453655" ID="ID_217271845" MODIFIED="1457901467474" TEXT="&#xf3;ra, perc m&#xe1;sodperc kett&#x151;sponttal elv&#xe1;lasztva">
+<node CREATED="1457901479255" ID="ID_1755189031" MODIFIED="1457901481840" TEXT="21:16:34">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1457901520696" ID="ID_1029210935" MODIFIED="1457901522518" TEXT="Ha a &#x201e;de.&#x201d; meghat&#xe1;roz&#xe1;s (d&#xe9;lel&#x151;tt), vagy &#x201e;du.&#x201d; (d&#xe9;lut&#xe1;n) r&#xf6;vid&#xed;t&#xe9;seket &#x2013; angolban &#x201e;am&#x201d; (ante meridiem) illetve &#x201e;pm&#x201d; (post meridiem) &#x2013; akarjuk haszn&#xe1;lni, akkor a 12 &#xf3;r&#xe1;s form&#xe1;tumot kell alkalmaznunk, p&#xe9;ld&#xe1;ul a 9:16:53pm helyes, viszont a du. 21:17:12 helytelen"/>
+<node CREATED="1457901484319" FOLDED="true" ID="ID_606453346" MODIFIED="1457901616035" TEXT="2015-01-02">
+<node CREATED="1457901548887" ID="ID_372650834" MODIFIED="1457901555318" TEXT="2015. janu&#xe1;r 2."/>
+<node CREATED="1457901559704" ID="ID_155583264" MODIFIED="1457901581788" TEXT="2015. jan. 2."/>
+<node CREATED="1457901562247" ID="ID_653294455" MODIFIED="1457901592079" TEXT="2015. I. 2."/>
+<node CREATED="1457901564511" ID="ID_240507096" MODIFIED="1457901601612" TEXT="2015. 01. 02."/>
+<node CREATED="1457901603263" ID="ID_1499643580" MODIFIED="1457901609950" TEXT="2015-01-02"/>
+</node>
+</node>
+<node CREATED="1457901620935" ID="ID_1769459669" MODIFIED="1457901629368" TEXT="K&#xf6;t&#x151;jelek &#xed;r&#xe1;sa">
+<node CREATED="1457901637303" ID="ID_692642839" MODIFIED="1457901643054" TEXT="Magyarban h&#xe1;romf&#xe9;le van">
+<node CREATED="1457901644368" ID="ID_1988613978" MODIFIED="1457901658982" TEXT="K&#xf6;t&#x151;jel -"/>
+<node CREATED="1457901659831" ID="ID_1647014857" MODIFIED="1457901671012" TEXT="Nagy k&#xf6;t&#x151;jel --"/>
+<node CREATED="1457901672440" ID="ID_930872102" MODIFIED="1457901678885" TEXT="Gondolatjel --"/>
+</node>
+<node CREATED="1457901690871" ID="ID_446432352" MODIFIED="1457901692911" TEXT="alkalmaz&#xe1;sa">
+<node CREATED="1457901700048" ID="ID_1502436090" MODIFIED="1457901701910" TEXT="elv&#xe1;laszt&#xe1;sn&#xe1;l"/>
+<node CREATED="1457901704696" ID="ID_748080710" MODIFIED="1457901714351" TEXT="sz&#xf3;kapcsolatokn&#xe1;l">
+<node CREATED="1457901722367" ID="ID_130557525" MODIFIED="1457901724826" TEXT="(h&#xe9;ba-h&#xf3;ba)">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
