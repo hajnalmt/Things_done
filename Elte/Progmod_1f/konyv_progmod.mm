a54922fa1610ca7ba9050f0e79e68eedf8cac097
Hajnal Máté <hajnalmt@gmail.com>
2016-03-16 15:47:14 +0100

    Progmod: Konyv in progress

--- Elte/Progmod_1f/konyv_progmod.mm
+++ Elte/Progmod_1f/konyv_progmod.mm
@@ -1,6 +1,6 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1457530905013" ID="ID_1385523408" MODIFIED="1457531419623" TEXT="Bevezet&#xe9;s a programoz&#xe1;shoz">
+<node CREATED="1457530905013" ID="ID_1385523408" MODIFIED="1458138922785" TEXT="Bevezet&#xe9;s a programoz&#xe1;shoz">
 <richcontent TYPE="NOTE"><html>
   <head>
     
@@ -21,7 +21,120 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1457531014862" ID="ID_1305375914" MODIFIED="1457531061155" POSITION="right" TEXT="1. Alapfogalmak">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1457531063935" ID="ID_1965263102" MODIFIED="1457531063935" TEXT=""/>
+<node CREATED="1457531063935" ID="ID_1965263102" MODIFIED="1458139158584" TEXT="Halmazok">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      N - a term&#233;szetes sz&#225;mok halmaza,
+    </p>
+    <p>
+      N 0 - a nemnegat&#237;v eg&#233;szek halmaza,
+    </p>
+    <p>
+      Z - az eg&#233;sz sz&#225;mok halmaza,
+    </p>
+    <p>
+      L - a logikai &#233;rt&#233;kek halmaza,
+    </p>
+    <p>
+      &#8709; - az &#252;res halmaz.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      a nemnegat&#237;v eg&#233;szek halmaza,
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      a logikai &#233;rt&#233;kek halmaza,
+    </p>
+    <p>
+      az &#252;res halmaz.
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1458139106674" ID="ID_636143593" MODIFIED="1458139253784" TEXT="megad&#xe1;suk">
+<node CREATED="1458139254210" ID="ID_340911373" MODIFIED="1458139294938" TEXT="elemeik  felsorol&#xe1;s&#xe1;val">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      L ::= {igaz, hamis},
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1458139261931" ID="ID_1990512079" MODIFIED="1458139303844" TEXT="egy tulajdons&#xe1;g megfolgalmaz&#xe1;s&#xe1;val">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      [a..b] ::= {x &#8712; Z | x &#8805; a &#233;s x &#8804; b}
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1458139305707" ID="ID_553371678" MODIFIED="1458139373750" TEXT="halmazelm&#xe9;leti m&#x171;veletek">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#8746; &#8722; uni&#243;,
+    </p>
+    <p>
+      &#8745; &#8722; metszet,
+    </p>
+    <p>
+      \ &#8722; k&#252;l&#246;nbs&#233;g
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1458139376715" ID="ID_1853617450" MODIFIED="1458139423969" TEXT="rel&#xe1;ci&#xf3;k">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#8712; &#8722; eleme,
+    </p>
+    <p>
+      &#8838; &#8722; r&#233;szhalmaza,
+    </p>
+    <p>
+      &#8834; &#8722; val&#243;di r&#233;sze.
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1458139443883" ID="ID_1464248870" MODIFIED="1458139459064" TEXT="sz&#xe1;moss&#xe1;g -  |H|">
+<node CREATED="1458139460403" ID="ID_1041983469" MODIFIED="1458139470227" TEXT="V&#xe9;ges sz&#xe1;moss&#xe1;g - |H| &lt; &#x221e;."/>
+</node>
+<node CREATED="1458139481338" ID="ID_284261828" MODIFIED="1458139515041" TEXT="r&#xe9;szhalmazaiknak halmaza (hatv&#xe1;nyhalmaz) - &#x2118;(H) "/>
+<node CREATED="1458139499707" ID="ID_1353014323" MODIFIED="1458139550270" TEXT="v&#xe9;ges r&#xe9;szhalmazainak halmaza - F(H)"/>
+</node>
 </node>
 <node CREATED="1457531071847" ID="ID_409542244" MODIFIED="1457531091539" POSITION="right" TEXT="2. A programoz&#xe1;s alapfogalmai">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
