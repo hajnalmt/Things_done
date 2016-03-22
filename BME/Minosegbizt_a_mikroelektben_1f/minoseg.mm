cf206a34431c1490dc9119c43bb520f8c981b6e8
Hajnal Máté <hajnalmt@gmail.com>
2016-03-22 11:41:35 +0100

    Minoseg: 0th phase

--- BME/Minosegbizt_a_mikroelektben_1f/minoseg.mm
+++ BME/Minosegbizt_a_mikroelektben_1f/minoseg.mm
@@ -4,7 +4,17 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1458642823212" ID="ID_677181218" MODIFIED="1458642870828" POSITION="right" TEXT="0) A min&#x151;s&#xe9;g fogalma, a min&#x151;s&#xe9;gbiztos&#xed;t&#xe1;si elvek &#xe9;s rendszerek fejl&#x151;d&#xe9;se">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1458642877727" ID="ID_846319208" MODIFIED="1458642889941" TEXT="A min&#x151;s&#xe9;g fogalma"/>
+<node CREATED="1458642877727" ID="ID_846319208" MODIFIED="1458642889941" TEXT="A min&#x151;s&#xe9;g fogalma">
+<node CREATED="1458642954968" ID="ID_98210011" MODIFIED="1458642960837" TEXT="Aggreg&#xe1;lt fogalom"/>
+<node CREATED="1458642969227" ID="ID_1404530638" MODIFIED="1458642988225" TEXT="M&#xfa;szaki-jogi &#xe9;rtelemben a specifik&#xe1;ci&#xf3; alapozza meg"/>
+<node CREATED="1458642991688" ID="ID_947712239" MODIFIED="1458643039089" TEXT="&#x170;zletpolitikailag a vev&#x151;i el&#xe9;gedetts&#xe9;g a d&#xf6;nt&#x151;"/>
+<node CREATED="1458643042253" ID="ID_701411429" MODIFIED="1458643060797" TEXT="A kiterjedt nemzetk&#xfc;zi kooper&#xe1;ci&#xf3; k&#xf6;vetelm&#xe9;nyei"/>
+<node CREATED="1458643095442" ID="ID_1253899246" MODIFIED="1458643117611" TEXT="Min&#x151;s&#xe9;g (Quality)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458643119608" ID="ID_1594439731" MODIFIED="1458643219465" TEXT="Egy term&#xe9;k, vagy szolg&#xe1;ltat&#xe1;s min&#x151;s&#xe9;ge az a tulajdons&#xe1;ga, hogy mennyire fele meg a felhaszn&#xe1;l&#xe1;s pillanat&#xe1;ban a vev&#x151;i ig&#xe9;nyeknek, vagy egy r&#xf6;gz&#xed;tett k&#xf6;vetelem&#xe9;nyrendszernek, &#xe9;s mennyire marad ilyen &#xe1;llapotban meghat&#xe1;roztt &#xe9;lettartam &#xe9;s ig&#xe9;nybev&#xe9;tel ut&#xe1;n."/>
+<node CREATED="1458643221408" ID="ID_294075089" MODIFIED="1458643221408" TEXT=""/>
+</node>
+</node>
 </node>
 </node>
 </map>
