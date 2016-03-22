6259daa8d57da865b21cb88ea7a773ae4305dfd6
Hajnal Máté <hajnalmt@gmail.com>
2016-03-22 11:35:29 +0100

    Minoseg mindmap initialized

---
+++ BME/Minosegbizt_a_mikroelektben_1f/minoseg.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1458642625411" ID="ID_1313638320" MODIFIED="1458642645103" TEXT="Min&#x151;s&#xe9;gbiztos&#xed;t&#xe1;s a mikroelektronik&#xe1;ban">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458642823212" ID="ID_677181218" MODIFIED="1458642870828" POSITION="right" TEXT="0) A min&#x151;s&#xe9;g fogalma, a min&#x151;s&#xe9;gbiztos&#xed;t&#xe1;si elvek &#xe9;s rendszerek fejl&#x151;d&#xe9;se">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1458642877727" ID="ID_846319208" MODIFIED="1458642889941" TEXT="A min&#x151;s&#xe9;g fogalma"/>
+</node>
+</node>
+</map>
