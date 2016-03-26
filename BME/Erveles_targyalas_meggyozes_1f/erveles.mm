160f22df656835094085e98485997da8eb91c84d
Hajnal Máté <hajnalmt@gmail.com>
2016-03-26 17:17:44 +0100

    Erveles mindmap 1. phase

--- BME/Erveles_targyalas_meggyozes_1f/erveles.mm
+++ BME/Erveles_targyalas_meggyozes_1f/erveles.mm
@@ -4,6 +4,24 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1459008586084" ID="ID_465546324" MODIFIED="1459008611321" POSITION="right" TEXT="1) Bevezet&#xe9;s, befoly&#xe1;sol&#xe1;s">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459008771416" ID="ID_731036492" MODIFIED="1459008785465" TEXT="Befoly&#xe1;sol&#xe1;s &#xe9;s t&#xed;pusai">
+<node CREATED="1459008796881" ID="ID_1247985553" MODIFIED="1459008837522" TEXT="A kommunik&#xe1;ci&#xf3; egyik c&#xe9;lja, hogy befoly&#xe1;soljuk a besz&#xe9;dpartnert">
+<node CREATED="1459008847672" ID="ID_653340656" MODIFIED="1459008864056" TEXT="Meger&#x151;s&#xed;ts&#xfc;k vagy megv&#xe1;ltoztassuk a v&#xe9;lem&#xe9;ny&#xe9;t"/>
+</node>
+<node CREATED="1459008872954" ID="ID_813972980" MODIFIED="1459008879272" TEXT="T&#xed;pusai">
+<node CREATED="1459008879778" ID="ID_90445926" MODIFIED="1459008888598" TEXT="meggy&#x151;z&#xe9;s"/>
+<node CREATED="1459008890149" ID="ID_400468154" MODIFIED="1459008895270" TEXT="manipul&#xe1;ci&#xf3;"/>
+<node CREATED="1459008896749" ID="ID_1196100994" MODIFIED="1459008900470" TEXT="k&#xe9;nyszer&#xed;t&#xe9;s"/>
+</node>
+<node CREATED="1459008903449" ID="ID_1838814714" MODIFIED="1459008932133" TEXT="Gyakran nem tiszta form&#xe1;ban jelennek meg pl: a manipul&#xe1;ci&#xf3; &#xe9;s a meggy&#xfa;z&#xe9;s keveredik"/>
+</node>
+<node CREATED="1459008787751" ID="ID_1557048768" MODIFIED="1459008949833" TEXT="Meggy&#x151;z&#xe9;s">
+<node CREATED="1459008951129" ID="ID_1763654277" MODIFIED="1459008978248" TEXT="A k&#xf6;zl&#x151; sz&#xe1;nd&#xe9;ka ismert a befogad&#xf3; el&#x151;tt"/>
+<node CREATED="1459008980110" ID="ID_928607582" MODIFIED="1459008989728" TEXT="Eszk&#xf6;ze a racion&#xe1;lis &#xe9;rvel&#xe9;s">
+<node CREATED="1459008995800" ID="ID_121110312" MODIFIED="1459009002211" TEXT="Logika, &#xe9;sz&#xe9;rvek"/>
+</node>
+<node CREATED="1459008991608" ID="ID_1128939258" MODIFIED="1459009036916" TEXT="Mindk&#xe9;t f&#xe9;l r&#xe9;sz&#xe9;t&#x151;l tudatos tev&#xe9;kenys&#xe9;get k&#xed;v&#xe1;n, k&#xe9;toldal&#xfa; folyamat"/>
+</node>
 </node>
 </node>
 </map>
