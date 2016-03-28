c366c12b506efb141725f72e9981bc24d5116f5c
Hajnal Máté <hajnalmt@gmail.com>
2016-03-28 12:44:29 +0200

    Linalg 1. phase proceeding

--- BME/Felmat_Linalg_1f/LinAlg.mm
+++ BME/Felmat_Linalg_1f/LinAlg.mm
@@ -4,7 +4,7 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1459156487977" ID="ID_1554243761" MODIFIED="1459156601566" POSITION="right" TEXT="1) Egyenletrendszerek, vektorok (a line&#xe1;ris algebra forr&#xe1;sai)">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1459156608018" ID="ID_718278855" MODIFIED="1459156635238" TEXT="Vektor">
+<node CREATED="1459156608018" FOLDED="true" ID="ID_718278855" MODIFIED="1459160549313" TEXT="Vektor">
 <node CREATED="1459156706914" ID="ID_1518623961" MODIFIED="1459156719434" TEXT="A 2 &#xe9;s 3 dimenzi&#xf3;s t&#xe9;r vektorai">
 <node CREATED="1459156721643" ID="ID_896621507" MODIFIED="1459156817271" TEXT="Szabad vektor">
 <node CREATED="1459156827433" ID="ID_232326231" MODIFIED="1459156852117" TEXT="Ha az ir&#xe1;ny&#xed;tott szakasz a hal, a vektor a halraj"/>
@@ -38,10 +38,62 @@
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node COLOR="#006699" CREATED="1459157835944" ID="ID_1623173003" MODIFIED="1459158177806" TEXT="Az a_1, a_2 ... a_n vektorok line&#xe1;ris kombin&#xe1;ci&#xf3;j&#xe1;n egy c_1a_1+c_2a_2+..+c_ka_k alak&#xfa; vektort &#xe9;rt&#xfc;nk, ahol c_1,c_2,..,c_k val&#xf3;s sz&#xe1;mok. Azt mondjuk, hogy a v vektor el&#x151;&#xe1;ll az a_1,a_2,..,a_k vektorok line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt, ha vannak olyan c_1,c_2,...,c_n val&#xf3;s sz&#xe1;mok, hogy v=c_1a_1+c_2a_2+..+c_ka_k"/>
 </node>
+<node COLOR="#006699" CREATED="1459158285350" ID="ID_1696767472" MODIFIED="1459158342807" TEXT="Vektorok line&#xe1;ris f&#xfc;ggetlens&#xe9;ge (def)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#006699" CREATED="1459158343829" ID="ID_1545326815" MODIFIED="1459159126763" TEXT="Azt mondjuk, hogy egy v vektor line&#xe1;risan f&#xfc;ggetlen az a_1,a_2,..,a_n (n&gt;=1) vektorokt&#xf3;l, ha v nem fejezhet&#x151; ki e vektorok line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt. Azt mondjuk, hogy az a_1,a_2,..,a_n (n&gt;=2) vektorok line&#xe1;risan f&#xfc;ggetlenek, ha e vektorok egyike sem fejezhet&#x151; ki a t&#xf6;bbi line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt. Ha legal&#xe1;bb egyik&#xfc;k kifejezhet&#x151; a t&#xf6;bbi line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt, azaz legal&#xe1;bb egyik&#xfc;k line&#xe1;risan f&#xfc;gg a t&#xf6;bbit&#x151;l, akkor e vektorokat line&#xe1;risan &#xf6;sszef&#xfc;gg&#x151;knek nevezz&#xfc;k. Az egyetlen vektorb&#xf3;l &#xe1;ll&#xf3; vektorrendszert line&#xe1;risan f&#xfc;ggetlennek tekintj&#xfc;k, ha az nem a z&#xe9;rusvektor."/>
 </node>
 </node>
-<node CREATED="1459156654325" ID="ID_337565563" MODIFIED="1459156663078" TEXT="Algebrai strukt&#xfa;r&#xe1;k"/>
-<node CREATED="1459156664571" ID="ID_1625471072" MODIFIED="1459156676862" TEXT="Line&#xe1;ris egyenletrendszerek"/>
+<node CREATED="1459159206238" ID="ID_441947280" MODIFIED="1459159219839" TEXT="Vektorok koordin&#xe1;t&#xe1;s alakban">
+<node CREATED="1459159231422" ID="ID_1560716152" MODIFIED="1459159240457" TEXT="Pontok koordi&#xe1;t&#xe1;i"/>
+<node CREATED="1459159243414" ID="ID_1989038832" MODIFIED="1459159254305" TEXT="Vektorok koordin&#xe1;t&#xe1;i"/>
+</node>
+<node CREATED="1459159256971" ID="ID_1740769214" MODIFIED="1459159269740" TEXT="R^n">
+<node COLOR="#006699" CREATED="1459159285977" ID="ID_1786736238" MODIFIED="1459159853519" TEXT="Vektorok &#xf6;sszead&#xe1;sa &#xe9;s skal&#xe1;rral szorz&#xe1;sa R^n (def)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#006699" CREATED="1459159332611" ID="ID_1092453377" MODIFIED="1459159840426" TEXT="Legyen c eleme R egy tetsz&#x151;leges val&#xf3;s u=(u_1,u_2...u_n) &#xe9;s v=(v_1,v_2...v_n) az R^n t&#xe9;r k&#xe9;t tetsz&#x151;leges vektorok. E k&#xe9;t vektor &#xf6;sszeg&#xe9;t &#xe9;s egyik c-szeres&#xe9;t a k&#xf6;vetkez&#x151; k&#xe9;plettel defini&#xe1;ljuk">
+<node CREATED="1459159701715" ID="ID_967312078" MODIFIED="1459159794909" TEXT="u+v=(u1+v_1,u_2+v_2,..,u_n+v_n)"/>
+<node CREATED="1459159796491" ID="ID_274181550" MODIFIED="1459159836988" TEXT="c*u=(c*u_1,c*u_2,...,c*u_n)"/>
+</node>
+<node CREATED="1459159863894" ID="ID_1388165008" MODIFIED="1459159874830" TEXT="A vektorok tulajdons&#xe1;gai &#xe9;rv&#xe9;nyben maradnak!"/>
+</node>
+<node COLOR="#ff0000" CREATED="1459159908583" ID="ID_571453355" MODIFIED="1459159922522" TEXT="Line&#xe1;ris f&#xfc;ggetlens&#xe9;g">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff0000" CREATED="1459159949551" ID="ID_130486939" MODIFIED="1459160427393" TEXT="Tetsz&#x151;leges R^n-beli V={v_1,v_2,...,v_n}  vektorrendszere az al&#xe1;bbi k&#xe9;t &#xe1;ll&#xed;t&#xe1;s ekvivalens">
+<node COLOR="#ff0000" CREATED="1459159989295" ID="ID_232855473" MODIFIED="1459160429400" TEXT="V line&#xe1;risan f&#xfc;ggetlen azaz k&gt;1 eset&#xe9;n egyik vektora sem fejezhet&#x151; ki a t&#xf6;bbi line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt, k=1 eset&#xe9;n pedig a vektor nem z&#xe9;rusvektor"/>
+<node COLOR="#ff0000" CREATED="1459160178042" ID="ID_1046572898" MODIFIED="1459160428876" TEXT="A z&#xe9;rusvektor csak egyf&#xe9;lek&#xe9;pp - a trivi&#xe1;lis m&#xf3;don - &#xe1;ll el&#x151; V line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt. M&#xe1;sk&#xe9;nt fogalmazva, a c_1,c_2,..,c_k skal&#xe1;rokkal vett line&#xe1;ris kombin&#xe1;ci&#xf3; csak akkor lehet a nullvektor, azaz c_1*v_1+c_2*v_2+...+c_n*v_n = 0, csak akkor &#xe1;ll fennt, ha c_1=c_2=...=c_n=0"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1459156654325" FOLDED="true" ID="ID_337565563" MODIFIED="1459161461662" TEXT="Algebrai strukt&#xfa;r&#xe1;k">
+<node CREATED="1459160563256" ID="ID_1934438310" MODIFIED="1459160568855" TEXT="Test &#xe9;s gy&#x171;r&#x171;">
+<node COLOR="#006699" CREATED="1459160629951" ID="ID_1163561122" MODIFIED="1459160672701" TEXT="Egy legal&#xe1;bb k&#xe9;telem&#x171; halmazt testnek nevez&#xfc;nk, ha">
+<node COLOR="#006699" CREATED="1459160673934" ID="ID_1523356914" MODIFIED="1459160747182" TEXT="&#xe9;rtelmezve van F elemp&#xe1;rjain egy &#xf6;sszead&#xe1;s &#xe9;s szorz&#xe1;s nev&#x171; bin&#xe1;ris m&#x171;velet"/>
+<node COLOR="#006699" CREATED="1459160748475" ID="ID_453694589" MODIFIED="1459160825769" TEXT="az &#xf6;sszead&#xe1;s kommutat&#xed;v, asszociat&#xed;v, l&#xe9;tezik nullelem &#xe9;s minden elemnek l&#xe9;tezik ellentettje"/>
+<node COLOR="#006699" CREATED="1459160809166" ID="ID_1691354813" MODIFIED="1459160928364" TEXT="a szorz&#xe1;s kommutat&#xed;v, asszociat&#xed;v, l&#xe9;tezik egys&#xe9;gelem &#xe9;s a nullelemen k&#xed;v&#xfc;l minden elemnek l&#xe9;tezik multiplikat&#xed;v inverze (reciproka)"/>
+<node COLOR="#006699" CREATED="1459160942730" ID="ID_1901870476" MODIFIED="1459160956276" TEXT="az &#xf6;sszead&#xe1;s a szorz&#xe1;sra n&#xe9;zve disztribut&#xed;v"/>
+</node>
+<node COLOR="#338800" CREATED="1459160989632" ID="ID_1109999705" MODIFIED="1459161034170" TEXT="A nullelem &#xe9;s az egys&#xe9;gelem sz&#xfc;ks&#xe9;gk&#xe9;ppen k&#xfc;l&#xf6;nb&#xf6;z&#x151;"/>
+<node COLOR="#338800" CREATED="1459161008941" ID="ID_1934951989" MODIFIED="1459161031717" TEXT="0a=a0=0"/>
+<node COLOR="#338800" CREATED="1459161043397" ID="ID_1057683220" MODIFIED="1459161100326" TEXT="R, Q, C, Z_p (pr&#xed;mmodoldus&#xfa; marad&#xe9;koszt&#xe1;lyok: M&#xe1;s jel&#xf6;l&#xe9;sek: F_p, GF(p)"/>
+<node COLOR="#006699" CREATED="1459161116657" ID="ID_1361971320" MODIFIED="1459161154882" TEXT="Ha a testn&#xe9;l defini&#xe1;lt szorz&#xe1;s csak asszociat&#xed;v, gy&#x171;r&#x171;r&#x151;l van sz&#xf3;"/>
+<node COLOR="#006699" CREATED="1459161158206" ID="ID_962060572" MODIFIED="1459161184667" TEXT="Ha kommutat&#xed;v is akkor kommutat&#xed;v gy&#x171;r&#x171;r&#x151;l van sz&#xf3;"/>
+<node COLOR="#006699" CREATED="1459161186240" ID="ID_1516191434" MODIFIED="1459161230722" TEXT="Ha az asszociativit&#xe1;s mellett van egys&#xe9;geleme is, akkor egys&#xe9;gelemes gy&#x171;r&#x171;r&#x151;l van sz&#xf3;"/>
+<node COLOR="#338800" CREATED="1459161233583" ID="ID_237480690" MODIFIED="1459161268591" TEXT="Minden test gy&#x171;r&#x171;"/>
+<node COLOR="#338800" CREATED="1459161271147" ID="ID_1496979825" MODIFIED="1459161295044" TEXT="Z egys&#xe9;gelemes kommutat&#xed;v gy&#x171;r&#x171;, N nem gy&#x171;r&#x171;"/>
+<node COLOR="#338800" CREATED="1459161296843" ID="ID_984340818" MODIFIED="1459161343336" TEXT="A p&#xe1;ros sz&#xe1;mok kommutat&#xed;v gy&#x171;r&#x171;t alkotnak, de ez nem egys&#xe9;gelemes."/>
+<node COLOR="#338800" CREATED="1459161353466" ID="ID_735731759" MODIFIED="1459161381681" TEXT="A Z_m egys&#xe9;gelems kommutat&#xed;v gy&#x171;r&#x171;, &#xe9;s pontosan akkor test, ha m pr&#xed;m"/>
+<node COLOR="#338800" CREATED="1459161384853" ID="ID_320466780" MODIFIED="1459161418144" TEXT="A val&#xf3;s egy&#xfc;tthat&#x151;s polinomok egys&#xe9;gelemes kommutat&#xed;v gy&#x171;r&#x171;t alkotnak"/>
+</node>
+</node>
+<node CREATED="1459156664571" ID="ID_1625471072" MODIFIED="1459156676862" TEXT="Line&#xe1;ris egyenletrendszerek">
+<node CREATED="1459161464171" ID="ID_717310207" MODIFIED="1459161472880" TEXT="Sor &#xe9;s oszlopmodell"/>
+<node CREATED="1459161474159" ID="ID_676868886" MODIFIED="1459161511782" TEXT="Alakzatok egyenletei: egyenes, s&#xed;k, hipers&#xed;k">
+<node COLOR="#006699" CREATED="1459161524582" ID="ID_864242405" MODIFIED="1459161548546" TEXT="Alakzat implicit egyenletrendszere (def)">
+<node CREATED="1459161843465" ID="ID_287161936" MODIFIED="1459161843465" TEXT=""/>
+</node>
+</node>
+</node>
 <node CREATED="1459156681475" ID="ID_1852190255" MODIFIED="1459156690497" TEXT="Vektort&#xe9;r (light)"/>
 </node>
 </node>
