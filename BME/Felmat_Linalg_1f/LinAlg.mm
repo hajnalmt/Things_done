2cfb537a01559ef4c39b5cbe9eebfa1f0d2742e7
Hajnal Máté <hajnalmt@gmail.com>
2016-03-28 11:51:58 +0200

    Linalg Mindmap started, first phase in progress

--- BME/Felmat_Linalg_1f/LinAlg.mm
+++ BME/Felmat_Linalg_1f/LinAlg.mm
@@ -1,4 +1,48 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1458615581084" ID="ID_411259303" MODIFIED="1458615611272" TEXT="Fels&#x151;bb Matematika - Halad&#xf3; Line&#xe1;ris Algebra"/>
+<node CREATED="1458615581084" ID="ID_411259303" MODIFIED="1459156492030" TEXT="Fels&#x151;bb Matematika - Halad&#xf3; Line&#xe1;ris Algebra">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459156487977" ID="ID_1554243761" MODIFIED="1459156601566" POSITION="right" TEXT="1) Egyenletrendszerek, vektorok (a line&#xe1;ris algebra forr&#xe1;sai)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459156608018" ID="ID_718278855" MODIFIED="1459156635238" TEXT="Vektor">
+<node CREATED="1459156706914" ID="ID_1518623961" MODIFIED="1459156719434" TEXT="A 2 &#xe9;s 3 dimenzi&#xf3;s t&#xe9;r vektorai">
+<node CREATED="1459156721643" ID="ID_896621507" MODIFIED="1459156817271" TEXT="Szabad vektor">
+<node CREATED="1459156827433" ID="ID_232326231" MODIFIED="1459156852117" TEXT="Ha az ir&#xe1;ny&#xed;tott szakasz a hal, a vektor a halraj"/>
+<node CREATED="1459156853667" ID="ID_78077544" MODIFIED="1459156877828" TEXT="Ekvivalenciarel&#xe1;ci&#xf3;">
+<node CREATED="1459156878907" ID="ID_1925085980" MODIFIED="1459156951949" TEXT="K&#xe9;t ir&#xe1;ny&#xed;tott szakasz ekvivalens, hogyha az egyik a m&#xe1;sikba &quot;tolhat&#xf3;&quot;"/>
+</node>
+<node CREATED="1459156911385" ID="ID_643226734" MODIFIED="1459156920779" TEXT="A vektorok ekvivalenciaoszt&#xe1;lyok"/>
+</node>
+<node CREATED="1459156991119" ID="ID_1715510068" MODIFIED="1459156994440" TEXT="Orig&#xf3;">
+<node CREATED="1459156999974" ID="ID_656324762" MODIFIED="1459157010072" TEXT="A k&#xf6;z&#xf6;s kezd&#x151;pont"/>
+<node CREATED="1459157030772" ID="ID_1195488979" MODIFIED="1459157088231" TEXT="A pontok &#xe9;s a vektorok k&#xf6;zt k&#xf6;lcs&#xf6;n&#xf6;sen egy&#xe9;rtelm&#x171; megfeleltet&#xe9;s">
+<node CREATED="1459157089478" ID="ID_398939576" MODIFIED="1459157153575" TEXT="Egy P pontnak a az OP vektor felel meg"/>
+<node CREATED="1459157155474" ID="ID_634136872" MODIFIED="1459157161655" TEXT="az orig&#xf3;nak a nullvektor felel meg"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1459157243051" ID="ID_19877718" MODIFIED="1459157787048" TEXT="A vektorm&#xfa;veletek tulajdons&#xe1;gai">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#ff0000" CREATED="1459157269533" ID="ID_1159179239" MODIFIED="1459157433670" TEXT="Ha a, b &#xe9;s c a 2- vagy 3-dimenzi&#xf3;s t&#xe9;r tetsz&#x151;leges vektorai, 0 a z&#xe9;rusvektor &#xe9;s r,s k&#xe9;t tetsz&#x151;leges val&#xf3;s sz&#xe1;m, akkor f&#xf6;nn&#xe1;llnak az al&#xe1;bbi azonoss&#xe1;gok:">
+<node COLOR="#ff0000" CREATED="1459157453600" ID="ID_1166174613" MODIFIED="1459157639116" TEXT="a+b=b+a"/>
+<node COLOR="#ff0000" CREATED="1459157464727" ID="ID_1542297881" MODIFIED="1459157638584" TEXT="(a+b)+c=a+(b+c)"/>
+<node COLOR="#ff0000" CREATED="1459157489844" ID="ID_1903684995" MODIFIED="1459157638325" TEXT="a+0=a"/>
+<node COLOR="#ff0000" CREATED="1459157502470" ID="ID_674140654" MODIFIED="1459157638105" TEXT="a+(-a)=0"/>
+<node COLOR="#ff0000" CREATED="1459157532039" ID="ID_1732682694" MODIFIED="1459157637900" TEXT="r(sa)=(rs)a"/>
+<node COLOR="#ff0000" CREATED="1459157550943" ID="ID_342876372" MODIFIED="1459157637680" TEXT="r(a+b)=ra+rb"/>
+<node COLOR="#ff0000" CREATED="1459157568439" ID="ID_1974153039" MODIFIED="1459157637445" TEXT="(r+s)a=ra+sa"/>
+<node COLOR="#ff0000" CREATED="1459157609251" ID="ID_973409971" MODIFIED="1459157637193" TEXT="1a=a, 0a=0"/>
+</node>
+<node CREATED="1459157689691" ID="ID_1177908490" MODIFIED="1459157716712" TEXT="E tulajdons&#xe1;gok vezetnek a vektort&#xe9;r &#xe1;ltal&#xe1;nos fogalm&#xe1;hoz"/>
+</node>
+<node COLOR="#006699" CREATED="1459157769610" ID="ID_920785227" MODIFIED="1459157816560" TEXT="Line&#xe1;ris kombin&#xe1;ci&#xf3; (def)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#006699" CREATED="1459157835944" ID="ID_1623173003" MODIFIED="1459158177806" TEXT="Az a_1, a_2 ... a_n vektorok line&#xe1;ris kombin&#xe1;ci&#xf3;j&#xe1;n egy c_1a_1+c_2a_2+..+c_ka_k alak&#xfa; vektort &#xe9;rt&#xfc;nk, ahol c_1,c_2,..,c_k val&#xf3;s sz&#xe1;mok. Azt mondjuk, hogy a v vektor el&#x151;&#xe1;ll az a_1,a_2,..,a_k vektorok line&#xe1;ris kombin&#xe1;ci&#xf3;jak&#xe9;nt, ha vannak olyan c_1,c_2,...,c_n val&#xf3;s sz&#xe1;mok, hogy v=c_1a_1+c_2a_2+..+c_ka_k"/>
+</node>
+</node>
+</node>
+<node CREATED="1459156654325" ID="ID_337565563" MODIFIED="1459156663078" TEXT="Algebrai strukt&#xfa;r&#xe1;k"/>
+<node CREATED="1459156664571" ID="ID_1625471072" MODIFIED="1459156676862" TEXT="Line&#xe1;ris egyenletrendszerek"/>
+<node CREATED="1459156681475" ID="ID_1852190255" MODIFIED="1459156690497" TEXT="Vektort&#xe9;r (light)"/>
+</node>
+</node>
 </map>
