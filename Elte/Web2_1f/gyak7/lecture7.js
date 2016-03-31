5820361f15c177b5635de95301c2150aa9e52485
Hajnal Máté <hajnalmt@gmail.com>
2016-03-31 10:42:41 +0200

    Web2 Learning Canvas

--- Elte/Web2_1f/gyak7/lecture7.js
+++ Elte/Web2_1f/gyak7/lecture7.js
@@ -1,4 +1,5 @@
-"use strict"
+//Lecture 7 Canvas
+"use strict";
 
 function $(s) {
 	return document.querySelector(s);
@@ -8,12 +9,34 @@ var canvas = $("canvas");
 //ceruza
 var p = canvas.getContext("2d");
 
+p.save();
 //kitölti a
 p.fillStyle = "purple";
 //négyzetet
 p.fillRect(0, 0, 100, 100);
+
+//Állítsam vissza mindig? --Nem használjuk a save, restoret!
+//p.fillStyle = "black"
+p.restore();
 //keretezés
 p.strokeStyle("red");
 p.strokeRect(100, 0, 100, 100);
 
-p.arc(200, 0, 50, 0, 2 * Math.PI);
+p.beginPath();
+p.arc(250, 50, 50, 0, 2 * Math.PI);
+
+p.fill(); // p.stroke();
+
+p.beginPath();
+p.moveTo(0, 480);
+p.lineTo(640, 0);
+//Ha vonalat akarsz rajzolni az mindig stroke lesz
+p.stroke();
+
+//nincs triangle, vonalakat kell használni!
+p.beginPath();
+p.moveTo(300, 100);
+p.lineTo(400, 100);
+p.lineTo(350, 0);
+p.lineTo(300, 100);
+p.fillStyle();
