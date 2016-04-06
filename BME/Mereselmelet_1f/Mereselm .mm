69451f308966e5ee9ddb41a004bd772984898704
Hajnal Máté <hajnalmt@gmail.com>
2016-04-06 09:48:59 +0200

    Mereselm: Elso eloadas part done

--- BME/Mereselmelet_1f/Mereselm .mm
+++ BME/Mereselmelet_1f/Mereselm .mm
@@ -1,4 +1,190 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1459922657258" ID="ID_733910614" MODIFIED="1459922680435" TEXT="M&#xe9;r&#xe9;selm&#xe9;let "/>
+<node CREATED="1459922657258" ID="ID_733910614" MODIFIED="1459922680435" TEXT="M&#xe9;r&#xe9;selm&#xe9;let ">
+<node CREATED="1459923538271" FOLDED="true" ID="ID_1796051752" MODIFIED="1459923903913" POSITION="right" TEXT="0. M&#xe9;r&#xe9;selm&#xe9;let">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459923547422" ID="ID_686864694" MODIFIED="1459923572798" TEXT="&quot;Data Scinence&quot; n&#xe9;ven azonos&#xed;tott ismeretanyag"/>
+<node CREATED="1459923581830" ID="ID_604404171" MODIFIED="1459923611628" TEXT="Az &#xe9;rz&#xe9;kel&#x151;k megjel&#xe9;nese miatt rengeteg adat feldolgoz&#xe1;sa sz&#xfc;ks&#xe9;ges">
+<node CREATED="1459923616582" ID="ID_98330194" MODIFIED="1459923624966" TEXT="&quot;data scientist&quot; kell"/>
+</node>
+<node CREATED="1459923626397" ID="ID_1040862477" MODIFIED="1459923693845" TEXT="&quot;Data Science is about tranforming data into business value using math and algorithms&quot;"/>
+<node CREATED="1459923700046" ID="ID_932837245" MODIFIED="1459923710210" TEXT="Harvard business Review:">
+<node CREATED="1459923711046" ID="ID_1663849559" MODIFIED="1459923728731" TEXT="Data Scientist: Sexiest Job of the Century"/>
+</node>
+</node>
+<node CREATED="1459923737167" FOLDED="true" ID="ID_1334948067" MODIFIED="1459923901478" POSITION="left" TEXT="1. Eml&#xe9;keztet&#x151;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459923755701" ID="ID_270924438" MODIFIED="1459923790360" TEXT="M&#xe9;r&#xe9;selm&#xe9;let">
+<node CREATED="1459923762045" ID="ID_846334966" MODIFIED="1459923768379" TEXT="M&#xe9;r&#xe9;s &#xe9;s modellez&#xe9;s"/>
+<node CREATED="1459923769430" ID="ID_1054694244" MODIFIED="1459923774683" TEXT="Modellilleszt&#xe9;s"/>
+<node CREATED="1459923775590" ID="ID_889226468" MODIFIED="1459923779786" TEXT="M&#xe9;r&#xe9;si hib&#xe1;k">
+<node CREATED="1459923780606" ID="ID_1631259553" MODIFIED="1459923786395" TEXT="hibaterjed&#xe9;s"/>
+<node CREATED="1459923799495" ID="ID_317985032" MODIFIED="1459923810828" TEXT="m&#xe9;r&#x151;eszk&#xf6;z strukt&#xfa;r&#xe1;k"/>
+<node CREATED="1459923811533" ID="ID_1845603434" MODIFIED="1459923820536" TEXT="jel&#xe1;talak&#xed;t&#xf3;k hib&#xe1;i"/>
+<node CREATED="1459923823295" ID="ID_420140892" MODIFIED="1459923832200" TEXT="m&#x171;szerek pontoss&#xe1;ga"/>
+</node>
+<node CREATED="1459923837157" ID="ID_817217612" MODIFIED="1459923843106" TEXT="Elemi m&#xe9;r&#xe9;si m&#xf3;dszerek"/>
+<node CREATED="1459923844550" ID="ID_366850179" MODIFIED="1459923861884" TEXT="M&#xe9;r&#xe9;si sorozatok ki&#xe9;rt&#xe9;kel&#xe9;se"/>
+<node CREATED="1459923863454" ID="ID_1014950004" MODIFIED="1459923877827" TEXT="M&#xe9;r&#xe9;si bizonytalans&#xe1;g kifejez&#xe9;se"/>
+</node>
+</node>
+<node CREATED="1459923905679" FOLDED="true" ID="ID_720543973" MODIFIED="1459928018873" POSITION="right" TEXT="2. M&#xe9;r&#xe9;si elj&#xe1;r&#xe1;s">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459923913607" ID="ID_655606202" MODIFIED="1459923960241" TEXT="A megismer&#xe9;si folyamat r&#xe9;sze, amelyek sor&#xe1;n a rendelkez&#xe9;s&#xfc;nkre &#xe1;ll&#xf3; ismereteinket pontos&#xed;tjuk ill. b&#x151;v&#xed;tj&#xfc;k"/>
+<node CREATED="1459923962279" FOLDED="true" ID="ID_708851853" MODIFIED="1459926425858" TEXT="Val&#xf3;s&#xe1;g megragad&#xe1;sa">
+<node CREATED="1459923985638" ID="ID_1404062677" MODIFIED="1459923992248" TEXT="Jellemz&#x151;kre &#xe9;p&#xed;tve">
+<node CREATED="1459924005486" ID="ID_283961939" MODIFIED="1459924011947" TEXT="Melyek stabilit&#xe1;st mutatnak"/>
+<node CREATED="1459924025702" ID="ID_304001717" MODIFIED="1459924034795" TEXT="Absztrakci&#xf3; r&#xe9;v&#xe9;n"/>
+<node CREATED="1459924048830" ID="ID_1005211281" MODIFIED="1459924059796" TEXT="Kiemelt szerezepek">
+<node CREATED="1459924060567" ID="ID_853800524" MODIFIED="1459924066151" TEXT="&#xe1;llapotv&#xe1;ltoz&#xf3;k (x)"/>
+<node CREATED="1459924067207" ID="ID_45019657" MODIFIED="1459924072744" TEXT="param&#xe9;terek (a)"/>
+<node CREATED="1459924073623" ID="ID_1940532777" MODIFIED="1459924080530" TEXT="strukt&#xfa;r&#xe1;k (S)"/>
+</node>
+</node>
+<node CREATED="1459924166390" ID="ID_63655161" MODIFIED="1459924176309" TEXT="Val&#xf3;s&#xe1;g tere">
+<node CREATED="1459924306781" ID="ID_794940863" MODIFIED="1459924312151" TEXT="Konkr&#xe9;t &#xe9;rt&#xe9;kek"/>
+</node>
+<node CREATED="1459924238399" ID="ID_568158695" MODIFIED="1459924353532" TEXT="Lek&#xe9;pez&#xe9;sek tere (megfigyel&#xe9;s r&#xe9;v&#xe9;n)">
+<node CREATED="1459924327935" ID="ID_1956948474" MODIFIED="1459924343205" TEXT="M&#xe9;r&#xe9;si/jel&#xe1;tviteli csatorn&#xe1;n &#xe1;t"/>
+</node>
+<node CREATED="1459924272599" ID="ID_1707912575" MODIFIED="1459924374549" TEXT="D&#xf6;nt&#xe9;sek tere">
+<node CREATED="1459924375871" ID="ID_159398843" MODIFIED="1459924378788" TEXT="bizonytalans&#xe1;g"/>
+</node>
+</node>
+<node CREATED="1459924449199" FOLDED="true" ID="ID_1743040774" MODIFIED="1459926421318" TEXT="Megfigyel&#xe9;s determinisztikus csatorna eset&#xe9;n">
+<node CREATED="1459924496838" ID="ID_563159585" MODIFIED="1459924508812" TEXT="A &#xe1;llapot&#xe1;tmenet"/>
+<node CREATED="1459924510287" ID="ID_269621798" MODIFIED="1459924524237" TEXT="C megfigyel&#xe9;si m&#xe1;trix"/>
+<node CREATED="1459925232871" ID="ID_1529497531" MODIFIED="1459925247914" TEXT="G korrekzi&#xf3;s m&#xe1;trixot &#xfa;gy tervezz&#xfc;k meg">
+<node CREATED="1459925249000" ID="ID_310213556" MODIFIED="1459925271457" TEXT="hogy a bercs&#xfc;lt x^(n) -&gt; x(n)"/>
+</node>
+<node CREATED="1459925323446" ID="ID_337612766" MODIFIED="1459925327398" TEXT="F =A -GC"/>
+<node CREATED="1459925334359" ID="ID_395728222" MODIFIED="1459925344276" TEXT="epszilon(n) x^ &#xe9;s x k&#xfc;l&#xf6;nbs&#xe9;ge">
+<node CREATED="1459925347199" ID="ID_633992725" MODIFIED="1459925353855" TEXT="epszilon(n) -&gt; 0"/>
+</node>
+<node CREATED="1459925373741" ID="ID_184837274" MODIFIED="1459925391146" TEXT="epsz(n+1)=F*eosz(n)">
+<node CREATED="1459925391148" ID="ID_1126758682" MODIFIED="1459925402388" TEXT="F cs&#xf6;kkenti epsz hossz&#xe1;t minden l&#xe9;p&#xe9;sben">
+<node CREATED="1459925572339" ID="ID_1445711282" MODIFIED="1459925588127" TEXT="epsz(n+1)&lt;epsz(n)"/>
+</node>
+<node CREATED="1459925403734" ID="ID_298717364" MODIFIED="1459925410093" TEXT="=F kontrakt&#xed;v"/>
+</node>
+<node COLOR="#338800" CREATED="1459925496686" ID="ID_526858929" MODIFIED="1459925506920" TEXT="Megjegyz&#xe9;sek">
+<node CREATED="1459925508270" ID="ID_1891390822" MODIFIED="1459925538678" TEXT="epsz(n)-el kapcsolatos egyenl&#x151;tlens&#xe9;g a hibavektor hossz&#xe1;ra &#xe9;rtelemezend&#x151;">
+<node CREATED="1459925540894" ID="ID_1381238101" MODIFIED="1459925552628" TEXT="skal&#xe1;r esetben abszol&#xfa;t &#xe9;rt&#xe9;kre"/>
+</node>
+<node CREATED="1459925554775" ID="ID_1017523934" MODIFIED="1459925625504" TEXT="Az elt&#x171;n&#xe9;shez nem kell megk&#xf6;veteln&#xfc;nk a hiba monotonit&#xe1;s&#xe1;t, csak a hibarendszer stabilit&#xe1;s&#xe1;t">
+<node CREATED="1459925635772" ID="ID_1726611498" MODIFIED="1459925650072" TEXT="(k&#xfc;ls&#x151; gerjeszt&#xe9;s n&#xe9;lk&#xfc;li esetben a null&#xe1;hoz konverg&#xe1;l&#xe1;st)"/>
+<node CREATED="1459925662095" ID="ID_1589378583" MODIFIED="1459925723726" TEXT="UA. Stabil &#xe1;llapot el&#xe9;r&#xe9;s&#xe9;hez a rendszer a bels&#x151; energi&#xe1;j&#xe1;t leadja (disszip&#xe1;lja)">
+<node CREATED="1459925737071" ID="ID_1108539645" MODIFIED="1459925779752" TEXT="ha a disszip&#xe1;ci&#xf3; minden l&#xe9;p&#xe9;sben fenn&#xe1;ll akkor a hibavetor hossz&#xe1;nak cs&#xf6;kken&#xe9;se monoton folyamat"/>
+</node>
+</node>
+</node>
+<node CREATED="1459925794630" ID="ID_821488336" MODIFIED="1459925797707" TEXT="Esetek:">
+<node CREATED="1459925798878" ID="ID_195512895" MODIFIED="1459925806356" TEXT="F = A-GC=0">
+<node CREATED="1459925817535" ID="ID_1163156384" MODIFIED="1459925823180" TEXT="Ha C n&#xe9;gyzetes">
+<node CREATED="1459925832895" ID="ID_1995941498" MODIFIED="1459925864680" TEXT="azaz a megfigyel&#xe9;s &#xe9;pp annyi komponens&#x171; mint az &#xe1;llapotvektor"/>
+<node COLOR="#ff0000" CREATED="1459925853999" ID="ID_962532962" MODIFIED="1459925902198" TEXT="A megfigyel&#x151; egyetlen l&#xe9;p&#xe9;sben k&#xf6;vetni tudja a megfigyelt rendszert!"/>
+</node>
+</node>
+<node CREATED="1459925904230" ID="ID_1696388633" MODIFIED="1459925944180" TEXT="F^N=(A-GC)^N=0">
+<node CREATED="1459925932982" ID="ID_1706201902" MODIFIED="1459925952315" TEXT="A hibarendszer N l&#xe9;p&#xe9;sben konverg&#xe1;l"/>
+<node CREATED="1459925953559" ID="ID_1070741445" MODIFIED="1459925983902" TEXT="az ilyen F^N m&#xe1;trixok nemderogat&#xf3;rius nilpotens m&#xe1;trixok">
+<node COLOR="#338800" CREATED="1459925991382" ID="ID_839491651" MODIFIED="1459926391198" TEXT="Valamennyi saj&#xe1;t&#xe9;rt&#xe9;k&#xfc;k nulla"/>
+<node COLOR="#338800" CREATED="1459926340183" ID="ID_719227183" MODIFIED="1459926390756" TEXT="&#xe1;tviteli f&#xfc;ggv&#xe9;ny&#xfc;k  racion&#xe1;lis t&#xf6;rtf&#xfc;ggv&#xe9;ny, melynek minden p&#xf3;lusa az orig&#xf3;ban van"/>
+<node COLOR="#338800" CREATED="1459926382983" ID="ID_1680770644" MODIFIED="1459926390366" TEXT="FIR sz&#x171;r&#x151;k"/>
+</node>
+<node CREATED="1459926013309" ID="ID_869655073" MODIFIED="1459926027453" TEXT="FIR rendszerek">
+<node CREATED="1459926031238" ID="ID_260874860" MODIFIED="1459926045406" TEXT="Hiszen a kezdeti hiba v&#xe9;ges l&#xe9;p&#xe9;sben elt&#x171;nik"/>
+</node>
+<node COLOR="#338800" CREATED="1459926057714" ID="ID_1385565809" MODIFIED="1459926061887" TEXT="Megj">
+<node CREATED="1459926063574" ID="ID_1086584391" MODIFIED="1459926073240" TEXT="ha F^M=0 M&lt;N">
+<node CREATED="1459926073973" ID="ID_1947861665" MODIFIED="1459926089632" TEXT="derogat&#xf3;rius nilpotens m&#xe1;trix"/>
+<node CREATED="1459926090758" ID="ID_679257469" MODIFIED="1459926103384" TEXT="konvergencia kevesebb , mint N l&#xe9;p&#xe9;sben bek&#xf6;vetkezik"/>
+</node>
+</node>
+</node>
+<node CREATED="1459926105342" ID="ID_46490713" MODIFIED="1459926122158" TEXT="F^N !=(A-GC)^N">
+<node CREATED="1459926124830" ID="ID_880494809" MODIFIED="1459926153529" TEXT="A hibarendszer &#xe1;llapotvektorainak hossza exponenci&#xe1;lisan cs&#xf6;kken"/>
+<node CREATED="1459926154692" ID="ID_1982967693" MODIFIED="1459926177430" TEXT="Akkor stabil, ha a saj&#xe1;t&#xe9;rt&#xe9;kek az egys&#xe9;gsugar&#xfa; k&#xf6;r&#xf6;n bel&#xfc;l vannak"/>
+<node CREATED="1459926187783" ID="ID_1520061358" MODIFIED="1459926199205" TEXT="IIR (v&#xe9;gtelen impulzusv&#xe1;lasz&#xfa;ak)">
+<node CREATED="1459926200006" ID="ID_1708031026" MODIFIED="1459926208371" TEXT="v&#xe9;gtelen l&#xe9;p&#xe9;sben t&#x171;nik csak el a hiba"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1459926428848" FOLDED="true" ID="ID_264299031" MODIFIED="1459928016898" TEXT="Megfigyel&#xe9;s zajos csatorna eset&#xe9;n">
+<node CREATED="1459926438494" ID="ID_78970552" MODIFIED="1459926461125" TEXT="nem epsz(n)-&gt;0 az elv&#xe1;r&#xe1;sunk"/>
+<node CREATED="1459926463599" ID="ID_1777987525" MODIFIED="1459926575397" TEXT="hanem E[epsz(n+1)*epsz^T (n+1)] minimuma">
+<node CREATED="1459926560127" ID="ID_700367089" MODIFIED="1459926570943" TEXT="A hibarendszer &#xe1;llapotegyenlete">
+<node CREATED="1459926571679" ID="ID_1102465183" MODIFIED="1459926607013" TEXT="E[epsz(n+1)*epsz^T (n+1)]=F*E[epsz(n)*epsz^T (n)] *F^T"/>
+</node>
+</node>
+<node CREATED="1459926534839" ID="ID_429441211" MODIFIED="1459926653925" TEXT="K&#xe1;lm&#xe1;n prediktorn&#xe1;l ez a hibam&#xe1;trix jut k&#xf6;zponti szerephez"/>
+<node COLOR="#338800" CREATED="1459927441375" ID="ID_1117496994" MODIFIED="1459927449854" TEXT="Megj">
+<node CREATED="1459927308133" ID="ID_488658611" MODIFIED="1459927465249" TEXT="K&#xf6;z&#xf6;s gerjeszt&#xe9;ssel a liner&#xe1;is modellek v&#xe1;ltazatlanok">
+<node CREATED="1459927353262" ID="ID_972555081" MODIFIED="1459927361939" TEXT="Szuperpoz&#xed;ci&#xf3; miatt"/>
+</node>
+<node CREATED="1459927481007" ID="ID_583185356" MODIFIED="1459927498703" TEXT="Luenberger megfigyel&#x151;">
+<node CREATED="1459927499311" ID="ID_433953276" MODIFIED="1459927526261" TEXT="egyetlen k&#xf6;vetelm&#xe9;ny:  megfigyel&#x151; legyen gyorsabb, mint a megfigyelt rendszer, k&#xfc;l&#xf6;nben nem tudja k&#xf6;vetni a v&#xe1;ltoz&#xe1;sokat"/>
+<node CREATED="1459927527359" ID="ID_42115077" MODIFIED="1459927532158" TEXT="Majdnem minden megfigyel&#x151;"/>
+</node>
+<node CREATED="1459927593312" ID="ID_7761032" MODIFIED="1459927603286" TEXT="ellen&#xe1;ll&#xe1;s- impedancia m&#xe9;r&#x151;">
+<node CREATED="1459927604031" ID="ID_1004353410" MODIFIED="1459927626739" TEXT="k&#xf6;ztes h&#xed;d&#xe1;g a megfigyel&#x151;/hangol&#xf3; modell"/>
+<node CREATED="1459927611982" ID="ID_1873166923" MODIFIED="1459927647164" TEXT="az ismeretlen elemes h&#xed;d&#xe1;g a val&#xf3;s&#xe1;g fizikai modellje"/>
+</node>
+</node>
+<node CREATED="1459927663279" FOLDED="true" ID="ID_329370312" MODIFIED="1459928015730" TEXT="Zajos  csatorna modellez&#xe9;se">
+<node CREATED="1459927670256" ID="ID_1813355570" MODIFIED="1459927689155" TEXT="val&#xf3;sz&#xed;n&#x171;s&#xe9;gi v&#xe1;ltoz&#xf3;k-sztochasztikus folyamatok"/>
+<node CREATED="1459927700015" ID="ID_745226809" MODIFIED="1459927707435" TEXT="X(w)">
+<node CREATED="1459927707807" ID="ID_1710361222" MODIFIED="1459927747805" TEXT="val v&#xe1;ltoz&#xf3; mely az esem&#xe9;nyt&#xe9;r w(omega) esem&#xe9;nyeihez val&#xf3;s sz&#xe1;mokat rendel."/>
+</node>
+<node CREATED="1459927753775" ID="ID_185026078" MODIFIED="1459927755486" TEXT="hisztogramm">
+<node CREATED="1459927767568" ID="ID_906077049" MODIFIED="1459927780700" TEXT="A s&#x171;r&#x171;s&#xe9;gf&#xfc;ggv&#xe9;ny statisztikai jellemz&#xe9;se"/>
+<node CREATED="1459927790663" ID="ID_1593043035" MODIFIED="1459927801328" TEXT="a v&#xe9;gtelens&#xe9;gig finom&#xed;tva megkapjuk a s&#x171;r&#x171;s&#xe9;gf&#xfc;ggv&#xe9;nyt">
+<node CREATED="1459927811904" ID="ID_1854293211" MODIFIED="1459927823461" TEXT="Ennek integr&#xe1;lja egy adott pontig az eloszl&#xe1;sf&#xfc;ggv&#xe9;ny"/>
+</node>
+</node>
+<node CREATED="1459927831872" ID="ID_1431921805" MODIFIED="1459927835678" TEXT="eloszl&#xe1;sf&#xfc;ggv&#xe9;ny">
+<node CREATED="1459927836191" ID="ID_1790618014" MODIFIED="1459927862707" TEXT="megmondja mennyi annak a vl&#xf3;sz&#xed;n&#x171;s&#xe9;ge, hogy a v&#xe1;ltoz&#xf3; &#xe9;rt&#xe9;ke egy pontn&#xe1;l nem nagyobb"/>
+</node>
+<node CREATED="1459927870600" ID="ID_561577050" MODIFIED="1459927889748" TEXT="x(t, w)-sztochasztikus folyamat">
+<node CREATED="1459927924902" ID="ID_1537033826" MODIFIED="1459927937189" TEXT="w esem&#xe9;nyekhez val&#xf3;s id&#x151;f&#xfc;ggv&#xe9;nyeket rendel"/>
+<node CREATED="1459927944840" ID="ID_1416535029" MODIFIED="1459927972412" TEXT="addott t-beli &#xe9;rt&#xe9;kek val&#xf3;sz&#xed;n&#x171;s&#xe9;gi v&#xe1;ltoz&#xf3;t reprezent&#xe1;lnak"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1459928000223" ID="ID_450386942" MODIFIED="1459928011921" POSITION="left" TEXT="3. D&#xf6;nt&#xe9;selm&#xe9;let alapjai">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1459928030294" ID="ID_1416123869" MODIFIED="1459928046149" TEXT="Bin&#xe1;ris/K&#xe9;thipot&#xe9;zises d&#xf6;nt&#xe9;s">
+<node CREATED="1459928097550" ID="ID_548447087" MODIFIED="1459928113035" TEXT="Ugyanarr&#xf3;l a jelens&#xe9;gr&#x151;l elt&#xe9;r&#x151; &#xe9;rt&#xe9;k&#x171; megfigyel&#xe9;seket kapunk"/>
+<node CREATED="1459928139032" ID="ID_1741401363" MODIFIED="1459928154021" TEXT="Rep&#xfc;l&#x151; eset- van-e ellens&#xe9;ges objektum"/>
+<node CREATED="1459928164894" ID="ID_212146709" MODIFIED="1459928182820" TEXT="megfigyel&#xe9;sek hisztogrammja-felt&#xe9;teles s&#x171;r&#x171;s&#xe9;gf&#xfc;ggv&#xe9;nyek"/>
+<node CREATED="1459928202520" ID="ID_1862182520" MODIFIED="1459928210462" TEXT="s&#x171;r&#x171;s&#xe9;gf&#xfc;ggv&#xe9;nyek egym&#xe1;shoz val&#xf3; viszonya"/>
+<node CREATED="1459928230216" ID="ID_101024031" MODIFIED="1459928239372" TEXT="KERESS&#xdc;K: D&#xf6;nt&#xe9;si k&#xfc;sz&#xf6;b">
+<node CREATED="1459928286088" ID="ID_1585257291" MODIFIED="1459928291437" TEXT="nem trivi&#xe1;lis"/>
+</node>
+</node>
+<node CREATED="1459928297126" ID="ID_1410594607" MODIFIED="1459928327057" TEXT="K&#xe9;thipot&#xe9;zises Bayes d&#xf6;nt&#xe9;s">
+<node COLOR="#ff0000" CREATED="1459928327520" ID="ID_1472203677" MODIFIED="1459928431472" TEXT="Apriori val&#xf3;sz&#xed;n&#x171;s&#xe9;gek ismerete"/>
+<node COLOR="#ff0000" CREATED="1459928339368" ID="ID_592969622" MODIFIED="1459928431839" TEXT="Csatornakarakterisztik&#xe1;k ismerete"/>
+<node CREATED="1459928427327" ID="ID_1366509278" MODIFIED="1459928438005" TEXT="K&#xf6;lts&#xe9;gek defini&#xe1;l&#xe1;sa">
+<node CREATED="1459928441792" ID="ID_848454882" MODIFIED="1459928453485" TEXT="C_ij - i-ediket fogadtuk el de j igaz"/>
+</node>
+<node CREATED="1459928472440" ID="ID_1678355160" MODIFIED="1459928481359" TEXT="C&#xe9;l: kock&#xe1;zat/k&#xf6;lts&#xe9;g minimaliz&#xe1;l&#xe1;sa">
+<node CREATED="1459928494912" ID="ID_1716862482" MODIFIED="1459928496213" TEXT="R &#xf03d; C 00 P 0 P ( H 0 H 0 ) + C 10 P 0 P ( H 1 H 0 ) + C 01 P 1 P ( H 0 H 1 ) + C 11 P 1 P ( H 1 H 1 )"/>
+<node CREATED="1459928538128" ID="ID_1874729679" MODIFIED="1459928576142" TEXT="Z_i elfogad&#xe1;si tartom&#xe1;ny"/>
+</node>
+<node CREATED="1459928646806" ID="ID_774265538" MODIFIED="1459928668668" TEXT="A d&#xf6;nt&#xe9;si k&#xfc;sz&#xf6;b &#xe9;rt&#xe9;k&#xe9;n&#xe9;l a k&#xe9;t s&#x171;r&#x171;s&#xe9;gf&#xfc;ggv&#xe9;ny h&#xe1;nyadosa egy el&#x151;re adott konstans"/>
+<node COLOR="#ff0000" CREATED="1459928706352" ID="ID_1931775060" MODIFIED="1459928764448" TEXT="likelihood ar&#xe1;ny ">
+<node COLOR="#ff0000" CREATED="1459928729616" ID="ID_1878350048" MODIFIED="1459928764142" TEXT="Bayes d&#xf6;nt&#xe9;si szab&#xe1;ly"/>
+</node>
+<node COLOR="#338800" CREATED="1459928785310" ID="ID_1282405886" MODIFIED="1459928794255" TEXT="Megjegyz&#xe9;sek">
+<node CREATED="1459928797040" ID="ID_1316945508" MODIFIED="1459928826638" TEXT="posteriori val&#xf3;sz&#xed;n&#x171;s&#xe9;gi d&#xf6;nt&#xe9;s">
+<node CREATED="1459928828671" ID="ID_400658834" MODIFIED="1459928842981" TEXT="n=P_0/P_1"/>
+</node>
+<node CREATED="1459928857424" ID="ID_653462855" MODIFIED="1459928867611" TEXT="szok&#xe1;s log-likelihoodot is haszn&#xe1;lni"/>
+</node>
+</node>
+</node>
+</node>
 </map>
