eb7cbe811e25aeb1be90fc7f46358e126f81eabf
Hajnal Máté <hajnalmt@gmail.com>
2016-04-06 08:06:54 +0200

    Mereselmélet: Mindmap initial phase

---
+++ BME/Mereselmelet_1f/Mereselm .mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1459922657258" ID="ID_733910614" MODIFIED="1459922680435" TEXT="M&#xe9;r&#xe9;selm&#xe9;let "/>
+</map>
