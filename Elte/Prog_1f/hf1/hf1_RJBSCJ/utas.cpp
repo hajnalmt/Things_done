8dc76c0e6369f2e62b7f22f4b76930c5f9c2aa4c
Hajnal Máté <hajnalmt@gmail.com>
2016-04-14 04:56:08 +0200

    Prog hf initial phase

---
+++ Elte/Prog_1f/hf1/hf1_RJBSCJ/utas.cpp
@@ -0,0 +1,16 @@
+//Készítette:    Hajnal Máté
+//Dátum:         2016.március.17
+//eha kód:       RJBSCJ
+//mail:          hajnalmt@gmail.com
+//csoport:       5.csoport
+//Feladat:       1.beadandó/11.feladat
+//               Egy határállomáson feljegyezték az átlépő utasok útlevélszámát. Melyik útlevélszámú
+//               utas fordult meg leghamarabb másodszor a határon?
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
