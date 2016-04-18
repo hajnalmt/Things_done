7ec77cf4ff341a351e61bdccd7c86fe34e46970c
Hajnal Máté <hajnalmt@gmail.com>
2016-04-18 10:59:46 +0200

    Prog mindmap initialized!

---
+++ Elte/Prog_1f/Programozas.mm
@@ -0,0 +1,12 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1460969754028" ID="ID_604382331" MODIFIED="1460969819895" TEXT="Programoz&#xe1;s">
+<node CREATED="1460969821136" ID="ID_269288944" MODIFIED="1460969831960" POSITION="right" TEXT="Programoz&#xe1;si t&#xe9;telek felsorol&#xf3;kra">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1460969869333" ID="ID_1552538188" MODIFIED="1460969880053" TEXT="T&#xed;pus szerkezet">
+<node CREATED="1460969880556" ID="ID_590285044" MODIFIED="1460969883064" TEXT="elemi t&#xed;pusok"/>
+<node CREATED="1460969884145" ID="ID_1313944457" MODIFIED="1460969888875" TEXT="&#xf6;sszetett t&#xed;pusok"/>
+</node>
+</node>
+</node>
+</map>
