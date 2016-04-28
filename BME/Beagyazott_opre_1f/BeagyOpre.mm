80cea41a6e9f1376569ca2f836d4cc434ff4eb50
Hajnal Máté <hajnalmt@gmail.com>
2016-04-28 19:49:49 +0200

    BeagyOpre mindmap initialized

---
+++ BME/Beagyazott_opre_1f/BeagyOpre.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1461865652258" ID="ID_667829746" MODIFIED="1461865721911" TEXT="Be&#xe1;gyazott oper&#xe1;ci&#xf3;s rendszerek">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</map>
