2b6cfa2bddd23cca71a1ca431de7bfce07e75ea1
Hajnal Máté <hajnalmt@gmail.com>
2016-05-09 22:18:02 +0200

    Daily Refresh on timestamp

--- Other/System_scripts/commit.sh
+++ Other/System_scripts/commit.sh
@@ -3,7 +3,7 @@
 #This is a script which automazies git commit method
 PATH_TO_PROJECT="/home/hunolulu/Repos_Mate/Things_done/"
 cd "$PATH_TO_PROJECT"
-echo "$PWD"
+
 commit_message="$1"
 git add . -A
 git commit -m "$commit_message"
