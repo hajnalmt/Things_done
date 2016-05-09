f49d393b248a97716717d204ed22c00e7d902a01
Hajnal Máté <hajnalmt@gmail.com>
2016-05-09 22:02:44 +0200

    Daily refresh

--- Other/System_scripts/commit.sh
+++ Other/System_scripts/commit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 #This is a script which automazies git commit method
+PATH_TO_PROJECT="/home/hunolulu/Repos_Mate/Things_done/"
+cd "$PATH_TO_PROJECT"
+echo "$PWD"
 commit_message="$1"
 git add . -A
-git commit "-m $commit_message"
+git commit -m "$commit_message"
 git push
