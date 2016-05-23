1c1390af1863d502ad133b0dda0efe86546cf687
Hajnal Máté <hajnalmt@gmail.com>
2016-05-23 04:26:45 +0200

    Minor change on daily_fresh script

--- Other/System_scripts/daily_fresh.sh
+++ Other/System_scripts/daily_fresh.sh
@@ -2,4 +2,4 @@
 
 date > /home/hunolulu/Repos_Mate/Things_done/Other/System_scripts/Time_stamp
 
-/home/hunolulu/Repos_Mate/Things_done/Other/System_scripts/commit.sh "Daily Refresh on timestamp"
+/home/hunolulu/Repos_Mate/Things_done/Other/System_scripts/commit.sh "Commit everything necessary"
