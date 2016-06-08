825f084ca65088871d9916f105027e02c84adae0
Hajnal Máté <hajnalmt@gmail.com>
2016-06-08 21:12:03 +0200

    SzamLatRend learning

--- BME/Szamitogepes_latrendszerek_1f/SzamLatRend.mm
+++ BME/Szamitogepes_latrendszerek_1f/SzamLatRend.mm
@@ -1,4 +1,35 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1465397811030" ID="ID_1674655376" MODIFIED="1465397845012" TEXT="SzamLatRend"/>
+<node CREATED="1465397811030" ID="ID_1674655376" MODIFIED="1465399010558" TEXT="SzamLatRend">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465399011850" ID="ID_1281635146" MODIFIED="1465399030195" POSITION="right" TEXT="I. A 2D k&#xe9;pfeldolgoz&#xe1;s alapjai">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465399035722" ID="ID_432972438" MODIFIED="1465399050065" TEXT="Sz&#xe1;m&#xed;t&#xf3;g&#xe9;pes l&#xe1;t&#xe1;s alapok">
+<node CREATED="1465399397279" ID="ID_824510851" MODIFIED="1465399399968" TEXT="1. Ismertesse a k&#xe9;p fogalm&#xe1;t: mire vagyunk k&#xed;v&#xe1;ncsiak? Mit nevez&#xfc;nk k&#xe9;pnek? Milyen k&#xe9;ple&#xed;r&#xe1;si lehet&#x151;s&#xe9;gek &#xe1;llnak rendelkez&#xe9;s&#xfc;nkre? Mit jelent a videofolyam?">
+<node CREATED="1465405752294" ID="ID_1880057329" MODIFIED="1465405759471" TEXT="K&#xe9;p: Vizu&#xe1;lis inform&#xe1;ci&#xf3;kat tartalmaz&#xf3; &#xf6;sszef&#xfc;gg&#x151; adathalmaz."/>
+<node CREATED="1465405765043" ID="ID_290119153" MODIFIED="1465405769319" TEXT="Vide&#xf3;folyam: Tipikusan k&#xe9;pek sorozata, mozg&#xe1;sle&#xed;r&#xe1;s (pl. MPEG4 t&#xf6;m&#xf6;r&#xed;t&#xe9;s) c&#xed;mesem&#xe9;nyek sorozata. &#xc1;ltal&#xe1;ban ezt dolgozzuk fel, van (a priori) ismeret&#xfc;nk az el&#x151;z&#x151; k&#xe9;pr&#x151;l, v&#xe1;ltoz&#xe1;st elemezz&#xfc;k."/>
+</node>
+</node>
+<node CREATED="1465399053255" ID="ID_1697754014" MODIFIED="1465399058740" TEXT="Jav&#xed;t&#xe1;s k&#xe9;ptartom&#xe1;nyban"/>
+<node CREATED="1465399065088" ID="ID_1333460539" MODIFIED="1465399070536" TEXT="Frekvenciatartom&#xe1;nyban"/>
+<node CREATED="1465399072127" ID="ID_928763801" MODIFIED="1465399078170" TEXT="Bin&#xe1;ris k&#xe9;pek"/>
+<node CREATED="1465399079943" ID="ID_678498442" MODIFIED="1465399082743" TEXT="M&#xe9;r&#xe9;sek"/>
+</node>
+<node CREATED="1465399090795" ID="ID_601877595" MODIFIED="1465399104988" POSITION="left" TEXT="II. A 3D k&#xe9;pfeldolgoz&#xe1;s alapjai">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465399113539" ID="ID_1071678878" MODIFIED="1465399126291" TEXT="Matematikai alapok, Projekt&#xed;v geometria"/>
+<node CREATED="1465399130488" ID="ID_952979736" MODIFIED="1465399137091" TEXT="Kameramodellek, kalibr&#xe1;ci&#xf3;"/>
+<node CREATED="1465399143974" ID="ID_1876980175" MODIFIED="1465399150529" TEXT="3D rekonstrukci&#xf3;"/>
+<node CREATED="1465399151760" ID="ID_907448420" MODIFIED="1465399158270" TEXT="K&#xe9;pilleszt&#xe9;s"/>
+<node CREATED="1465399160385" ID="ID_1390726951" MODIFIED="1465399164314" TEXT="Tanul&#xf3; l&#xe1;t&#xe1;s"/>
+</node>
+<node CREATED="1465399171748" ID="ID_178493250" MODIFIED="1465399192565" POSITION="right" TEXT="III. Val&#xf3;sidej&#x171; l&#xe1;t&#xe1;s eszk&#xf6;zei, paradigm&#xe1;i">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1465399192977" ID="ID_730765938" MODIFIED="1465399207081" TEXT="A val&#xf3;sidej&#x171; l&#xe1;t&#xe1;s eszk&#xf6;zei"/>
+<node CREATED="1465399208504" ID="ID_764211038" MODIFIED="1465399214591" TEXT="GPGPU, CUDA"/>
+<node CREATED="1465399223200" ID="ID_247211429" MODIFIED="1465399229581" TEXT="K&#xe9;pfeldolgoz&#xe1;s mobileszk&#xf6;z&#xf6;kkel"/>
+<node CREATED="1465399241325" ID="ID_1417413276" MODIFIED="1465399250045" TEXT="L&#xe1;t&#xe1;s programozhat&#xf3; hardvereszk&#xf6;z&#xf6;kkel"/>
+<node CREATED="1465399253115" ID="ID_1706545883" MODIFIED="1465399261309" TEXT="CMOS alap&#xfa; el&#x151;feldolgoz&#xe1;s"/>
+</node>
+</node>
 </map>
