bc0077722d95acc0d16e4092f23361453871aeab
Hajnal Máté <hajnalmt@gmail.com>
2016-07-10 17:03:41 +0200

    Started refreshing html knowledge

---
+++ Other/Practice/webdev/webdev.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1468162684916" ID="ID_1101556271" MODIFIED="1468162729733" TEXT="Web Development">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1468162732615" ID="ID_835441060" MODIFIED="1468162740607" POSITION="right" TEXT="1. HTML">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1468162741197" ID="ID_503210694" MODIFIED="1468162741197" TEXT=""/>
+</node>
+</node>
+</map>
