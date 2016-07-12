ff96d007ad3daf9cde4fd62c0feb4986753e9132
Hajnal Máté <hajnalmt@gmail.com>
2016-07-12 02:15:06 +0200

    Finished html refreshing with Links phase

--- Other/Practice/webdev/webdev.mm
+++ Other/Practice/webdev/webdev.mm
@@ -5,7 +5,7 @@
 <node CREATED="1468162732615" ID="ID_835441060" MODIFIED="1468162740607" POSITION="right" TEXT="1. HTML">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1468162741197" ID="ID_503210694" MODIFIED="1468162794316" TEXT="Tutorial">
-<node CREATED="1468162794829" ID="ID_1025799643" MODIFIED="1468162798277" TEXT="Introduction">
+<node CREATED="1468162794829" FOLDED="true" ID="ID_1025799643" MODIFIED="1468277758965" TEXT="Introduction">
 <node CREATED="1468275331393" ID="ID_527244464" MODIFIED="1468275335034" TEXT="What is HTML?">
 <node CREATED="1468275340723" ID="ID_1393530167" MODIFIED="1468275341023" TEXT="HTML stands for Hyper Text Markup Language"/>
 </node>
@@ -15,11 +15,11 @@
 <node CREATED="1468275378091" ID="ID_1370619112" MODIFIED="1468275393128" TEXT="The &lt;!DOCTYPE&gt; declaration"/>
 <node CREATED="1468275401348" ID="ID_210475068" MODIFIED="1468275404107" TEXT="HTML versions"/>
 </node>
-<node CREATED="1468163147565" ID="ID_922786500" MODIFIED="1468163151298" TEXT="Editors">
+<node CREATED="1468163147565" FOLDED="true" ID="ID_922786500" MODIFIED="1468277757252" TEXT="Editors">
 <node CREATED="1468275420771" ID="ID_377693390" MODIFIED="1468275423089" TEXT="Notepad"/>
 <node CREATED="1468275431587" ID="ID_1005253861" MODIFIED="1468275441463" TEXT="How to open a web page?"/>
 </node>
-<node CREATED="1468268123939" ID="ID_149960750" MODIFIED="1468268133203" TEXT="Basic">
+<node CREATED="1468268123939" FOLDED="true" ID="ID_149960750" MODIFIED="1468277756214" TEXT="Basic">
 <node CREATED="1468275452379" ID="ID_944423848" MODIFIED="1468275457481" TEXT="HTML Documents">
 <node CREATED="1468275489692" ID="ID_1759052831" MODIFIED="1468275506289" TEXT="&lt;!DOCTYPE html&gt;"/>
 <node CREATED="1468275551779" ID="ID_729177272" MODIFIED="1468275579123">
@@ -49,7 +49,7 @@
 <node CREATED="1468275626859" ID="ID_907495422" MODIFIED="1468275649287" TEXT="src, alt size (width height) attributes"/>
 </node>
 </node>
-<node CREATED="1468268118656" ID="ID_1932117264" MODIFIED="1468268122322" TEXT="Elements">
+<node CREATED="1468268118656" FOLDED="true" ID="ID_1932117264" MODIFIED="1468280768333" TEXT="Elements">
 <node CREATED="1468275784603" ID="ID_1322886586" MODIFIED="1468275789066" TEXT="start and end tag">
 <node COLOR="#990000" CREATED="1468275814730" ID="ID_1532831217" MODIFIED="1468275823908" TEXT=" Do not forget end tag "/>
 <node CREATED="1468275825147" ID="ID_1167106437" MODIFIED="1468275832725" TEXT="empty elements">
@@ -62,7 +62,7 @@
 <node CREATED="1468275905227" ID="ID_1214350624" MODIFIED="1468275910313" TEXT="xhtml needs "/>
 </node>
 </node>
-<node CREATED="1468268127483" ID="ID_1337899090" MODIFIED="1468268130672" TEXT="Attributes">
+<node CREATED="1468268127483" FOLDED="true" ID="ID_1337899090" MODIFIED="1468280773110" TEXT="Attributes">
 <node CREATED="1468275920315" ID="ID_303479245" MODIFIED="1468275927561" TEXT="Always in the start tag "/>
 <node CREATED="1468275928299" ID="ID_1742321267" MODIFIED="1468275941912" TEXT="Additional information"/>
 <node CREATED="1468275955323" ID="ID_1447594012" MODIFIED="1468275956627" TEXT="Types">
@@ -86,7 +86,7 @@
 <node CREATED="1468276117483" ID="ID_434582793" MODIFIED="1468276125012" TEXT="both single and double is good&#x171;"/>
 </node>
 </node>
-<node CREATED="1468268135395" ID="ID_1693741163" MODIFIED="1468268137521" TEXT="Headings">
+<node CREATED="1468268135395" FOLDED="true" ID="ID_1693741163" MODIFIED="1468277761317" TEXT="Headings">
 <node CREATED="1468276140923" ID="ID_688663794" MODIFIED="1468276150984" TEXT="From h1 to h6 in priority"/>
 <node CREATED="1468276187107" ID="ID_1305926180" MODIFIED="1468276192441" TEXT="Horizontal rule">
 <node CREATED="1468276193515" ID="ID_923392703" MODIFIED="1468276200662" TEXT="hr tag"/>
@@ -98,7 +98,7 @@
 </node>
 <node CREATED="1468276290699" ID="ID_146384610" MODIFIED="1468276295032" TEXT="view source"/>
 </node>
-<node CREATED="1468268141123" ID="ID_1364918705" MODIFIED="1468268143920" TEXT="Paragraphs">
+<node CREATED="1468268141123" FOLDED="true" ID="ID_1364918705" MODIFIED="1468280787338" TEXT="Paragraphs">
 <node CREATED="1468276304787" ID="ID_1826497275" MODIFIED="1468276307849" TEXT="p element"/>
 <node CREATED="1468276329691" ID="ID_198173438" MODIFIED="1468276363329" TEXT="Browsers removes all the HTML whitespaces or extra lines"/>
 <node CREATED="1468276416602" ID="ID_1515069532" MODIFIED="1468276421960" TEXT="Don&apos;t forget the End Tag"/>
@@ -110,7 +110,7 @@
 <node CREATED="1468276473308" ID="ID_467747781" MODIFIED="1468276529194" TEXT="The text inside the &lt;pre&gt; element is displayed in a fixed-width (usually Courier), and it preserves both spaces and line breaks"/>
 </node>
 </node>
-<node CREATED="1468268144836" ID="ID_553221550" MODIFIED="1468268149535" TEXT="Styles">
+<node CREATED="1468268144836" FOLDED="true" ID="ID_553221550" MODIFIED="1468277764421" TEXT="Styles">
 <node CREATED="1468276544203" ID="ID_967880180" MODIFIED="1468276548482" TEXT="style attribute">
 <node CREATED="1468276557179" ID="ID_1585659287" MODIFIED="1468276588114" TEXT="&quot;property:value&quot;"/>
 <node CREATED="1468276568435" ID="ID_962306079" MODIFIED="1468276584513" TEXT="The property is a CSS property, the value is a CSS value"/>
@@ -128,8 +128,8 @@
 </node>
 </node>
 </node>
-<node CREATED="1468268150994" ID="ID_1034107415" MODIFIED="1468276967825" TEXT="Formatting">
-<node CREATED="1468276806795" ID="ID_1627976302" MODIFIED="1468276820308" TEXT="Elements">
+<node CREATED="1468268150994" FOLDED="true" ID="ID_1034107415" MODIFIED="1468277765732" TEXT="Formatting">
+<node CREATED="1468276806795" FOLDED="true" ID="ID_1627976302" MODIFIED="1468277225928" TEXT="Elements">
 <node CREATED="1468276840483" ID="ID_1165353634" MODIFIED="1468276847256" TEXT="b tag">
 <node CREATED="1468276849979" ID="ID_848688533" MODIFIED="1468276853781" TEXT="bold text"/>
 </node>
@@ -162,6 +162,146 @@
 </node>
 </node>
 <node CREATED="1468276968970" ID="ID_1315421735" MODIFIED="1468276973673" TEXT="Note: Browsers display &lt;strong&gt; as &lt;b&gt;, and &lt;em&gt; as &lt;i&gt;. However, there is a difference in the meaning of these tags: &lt;b&gt; and &lt;i&gt; defines bold and italic text, but &lt;strong&gt; and &lt;em&gt; means that the text is &quot;important&quot;."/>
+</node>
+<node CREATED="1468277770275" FOLDED="true" ID="ID_1829553315" MODIFIED="1468281766069" TEXT="Quotations">
+<node CREATED="1468279915234" ID="ID_1458014719" MODIFIED="1468279933536" TEXT="html &lt;q&gt; for short quotations"/>
+<node CREATED="1468279952914" ID="ID_1540563618" MODIFIED="1468279993383" TEXT="tag blockquote for sections from an other source"/>
+<node CREATED="1468279995171" ID="ID_1282842186" MODIFIED="1468280089248" TEXT="abbr tag for abbreviations or acronym"/>
+<node CREATED="1468280206811" ID="ID_30409003" MODIFIED="1468280223785" TEXT="address tag for contact information">
+<node CREATED="1468280298923" ID="ID_1136700962" MODIFIED="1468280299303" TEXT="The &lt;address&gt; element is usually displayed in italic. Most browsers will add a line break before and after the element."/>
+</node>
+<node CREATED="1468280322684" ID="ID_29259958" MODIFIED="1468280337747" TEXT="cite tag defines the title of a work"/>
+<node CREATED="1468280353331" ID="ID_1021605250" MODIFIED="1468280353640" TEXT="HTML &lt;bdo&gt; for Bi-Directional Override">
+<node CREATED="1468280371091" ID="ID_1303148199" MODIFIED="1468280371932" TEXT="The &lt;bdo&gt; element is used to override the current text direction:  "/>
+</node>
+</node>
+<node CREATED="1468277785779" FOLDED="true" ID="ID_1888108403" MODIFIED="1468281767237" TEXT="Computercode">
+<node CREATED="1468280550434" ID="ID_999121707" MODIFIED="1468280564771" TEXT="HTML normally uses variable letter size and spacing"/>
+<node CREATED="1468280567898" ID="ID_1119513676" MODIFIED="1468280640853" TEXT="This is not when we are about to displaying computer code">
+<node CREATED="1468280595587" ID="ID_203844766" MODIFIED="1468280614897" TEXT="kbd tag for keyboard input"/>
+<node CREATED="1468280617539" ID="ID_814811875" MODIFIED="1468280638082" TEXT="samp for computer ouptut"/>
+<node CREATED="1468280642459" ID="ID_1172559850" MODIFIED="1468280666489" TEXT="code tag for computer code">
+<node COLOR="#990000" CREATED="1468280684010" ID="ID_761646283" MODIFIED="1468280688075" TEXT=" &lt;code&gt; element does not preserve extra whitespace and line-breaks."/>
+<node CREATED="1468280693211" ID="ID_1917255328" MODIFIED="1468280701200" TEXT="To fix this use pre element"/>
+</node>
+<node CREATED="1468280721810" ID="ID_793628623" MODIFIED="1468280725920" TEXT="var tag for variables">
+<node CREATED="1468280750595" ID="ID_1206991250" MODIFIED="1468280750944" TEXT="The variable could be a variable in a mathematical expression or a variable in programming context:"/>
+</node>
+</node>
+</node>
+<node CREATED="1468277794635" FOLDED="true" ID="ID_356162932" MODIFIED="1468281769469" TEXT="Comments">
+<node CREATED="1468280825780" ID="ID_846445907" MODIFIED="1468280828066" TEXT="syntax">
+<node CREATED="1468280828562" ID="ID_1456047070" MODIFIED="1468280829189" TEXT="&lt;!-- Write your comments here --&gt;"/>
+</node>
+<node CREATED="1468280841763" ID="ID_363847255" MODIFIED="1468280842081" TEXT="Note: Comments are not displayed by the browser, but they can help document your HTML source code"/>
+<node CREATED="1468280868019" ID="ID_949484626" MODIFIED="1468280873665" TEXT="Conditional comments">
+<node CREATED="1468280874355" ID="ID_1603749279" MODIFIED="1468280875065" TEXT="&lt;!--[if IE 8]&gt;     .... some HTML here .... &lt;![endif]--&gt;"/>
+</node>
+</node>
+<node CREATED="1468277798067" FOLDED="true" ID="ID_1924678081" MODIFIED="1468281770285" TEXT="Colors">
+<node CREATED="1468280929019" ID="ID_1369108995" MODIFIED="1468280929603" TEXT="In HTML, a color can be specified by using a color name, an RGB value, or a HEX value."/>
+<node CREATED="1468280950922" ID="ID_47386225" MODIFIED="1468280958713" TEXT="140 standard color name"/>
+<node CREATED="1468280968738" ID="ID_385064895" MODIFIED="1468280972017" TEXT="rgb value">
+<node CREATED="1468281001554" ID="ID_131420258" MODIFIED="1468281015392" TEXT="rgb(R,G,B) 0-255 EACH"/>
+</node>
+<node CREATED="1468280979715" ID="ID_526954911" MODIFIED="1468280982577" TEXT="hex value">
+<node CREATED="1468280990083" ID="ID_303555272" MODIFIED="1468280994547" TEXT="#RRGGBB"/>
+</node>
+</node>
+<node CREATED="1468277801835" FOLDED="true" ID="ID_1894554802" MODIFIED="1468281772216" TEXT="CSS">
+<node CREATED="1468281047531" ID="ID_1346864124" MODIFIED="1468281056648" TEXT="CSS stands for Cascading Style Sheets."/>
+<node CREATED="1468281064491" ID="ID_205640289" MODIFIED="1468281064811" TEXT="CSS describes how HTML elements are to be displayed on screen, paper, or in other media."/>
+<node CREATED="1468281074618" ID="ID_1977666686" MODIFIED="1468281076066" TEXT="CSS saves a lot of work. It can control the layout of multiple web pages all at once."/>
+<node CREATED="1468281087907" ID="ID_857054323" MODIFIED="1468281088962" TEXT="CSS can be added to HTML elements in 3 ways:">
+<node CREATED="1468281090011" ID="ID_1802376981" MODIFIED="1468281094777" TEXT="Inline">
+<node CREATED="1468281095131" ID="ID_364710713" MODIFIED="1468281108009" TEXT="Using style attribute in HTML elements"/>
+<node CREATED="1468281178594" ID="ID_513976022" MODIFIED="1468281179299" TEXT="An inline CSS is used to apply a unique style to a single HTML element."/>
+</node>
+<node CREATED="1468281110642" ID="ID_809618268" MODIFIED="1468281112839" TEXT="Internal">
+<node CREATED="1468281130818" ID="ID_826201071" MODIFIED="1468281147937" TEXT="Using &lt;style&gt; element in head section"/>
+<node CREATED="1468281197211" ID="ID_1314670488" MODIFIED="1468281198184" TEXT="An internal CSS is used to define a style for a single HTML page."/>
+</node>
+<node CREATED="1468281113858" ID="ID_938070949" MODIFIED="1468281116562" TEXT="External">
+<node CREATED="1468281117770" ID="ID_151996610" MODIFIED="1468281129393" TEXT="Using an external CSS file"/>
+<node CREATED="1468281252211" ID="ID_715140467" MODIFIED="1468281252433" TEXT="With an external style sheet, you can change the look of an entire web site, by changing one file!"/>
+</node>
+</node>
+<node CREATED="1468281339907" ID="ID_1487286449" MODIFIED="1468281343970" TEXT="Elements">
+<node CREATED="1468281368059" ID="ID_67385534" MODIFIED="1468281369889" TEXT="color">
+<node CREATED="1468281370251" ID="ID_457671947" MODIFIED="1468281375730" TEXT="defines text-color"/>
+</node>
+<node CREATED="1468281388443" ID="ID_1062436534" MODIFIED="1468281391826" TEXT="font-family">
+<node CREATED="1468281376674" ID="ID_573189593" MODIFIED="1468281397357" TEXT="defines font to be used"/>
+</node>
+<node CREATED="1468281404874" ID="ID_1153147619" MODIFIED="1468281410240" TEXT="font-size">
+<node CREATED="1468281410642" ID="ID_1403158352" MODIFIED="1468281418688" TEXT="text size to be used"/>
+</node>
+<node CREATED="1468281436899" ID="ID_302676352" MODIFIED="1468281438719" TEXT="border">
+<node CREATED="1468281432635" ID="ID_1666053750" MODIFIED="1468281439899" TEXT="The CSS border property defines a border around an HTML element:"/>
+<node CREATED="1468281504251" ID="ID_1138125164" MODIFIED="1468281511711" TEXT="padding">
+<node CREATED="1468281512266" ID="ID_730789389" MODIFIED="1468281534388" TEXT="padding (space) between the text and border"/>
+</node>
+<node CREATED="1468281537483" ID="ID_698357046" MODIFIED="1468281541235" TEXT="margin">
+<node CREATED="1468281541891" ID="ID_14944548" MODIFIED="1468281566585" TEXT="margin (space) outside the border"/>
+</node>
+</node>
+<node CREATED="1468281624602" ID="ID_588495337" MODIFIED="1468281634081" TEXT="id attribute">
+<node CREATED="1468281634707" ID="ID_847753348" MODIFIED="1468281652786" TEXT="specific/special element, add an id attribute to the element"/>
+<node CREATED="1468281665987" ID="ID_510492810" MODIFIED="1468281672017" TEXT="#id in the css"/>
+</node>
+<node CREATED="1468281689179" ID="ID_1602672884" MODIFIED="1468281694353" TEXT="class attribute ">
+<node CREATED="1468281694667" ID="ID_392377892" MODIFIED="1468281703520" TEXT="special type of elements"/>
+<node CREATED="1468281726307" ID="ID_530612671" MODIFIED="1468281741384" TEXT=".class in the css"/>
+</node>
+</node>
+</node>
+<node CREATED="1468277808515" ID="ID_1548837472" MODIFIED="1468277810248" TEXT="Links">
+<node CREATED="1468281796467" ID="ID_1581940867" MODIFIED="1468281797149" TEXT="HTML links are hyperlinks."/>
+<node CREATED="1468281798907" ID="ID_832108543" MODIFIED="1468281808040" TEXT="You can click on a link and jump to another document. "/>
+<node CREATED="1468281815338" ID="ID_831441741" MODIFIED="1468281816330" TEXT="Note: A link does not have to be text. It can be an image or any other HTML element."/>
+<node CREATED="1468281825194" ID="ID_1186785793" MODIFIED="1468281828241" TEXT="syntax">
+<node CREATED="1468281829674" ID="ID_1864341710" MODIFIED="1468281868401" TEXT="defined with &lt;a&gt; tag"/>
+<node CREATED="1468281862259" ID="ID_1489057933" MODIFIED="1468281863017" TEXT="The href attribute specifies the destination address (http://www.w3schools.com/html/) of the link."/>
+<node CREATED="1468281883963" ID="ID_1933909371" MODIFIED="1468281884641" TEXT="The link text is the visible part"/>
+<node COLOR="#990000" CREATED="1468281907570" ID="ID_1842238935" MODIFIED="1468281922523" TEXT="Note: Without a forward slash on subfolder addresses, you might generate two requests to the server. Many servers will automatically add a forward slash to the address, and then create a new request."/>
+</node>
+<node CREATED="1468281949379" ID="ID_111127373" MODIFIED="1468281958849" TEXT="Local link with relative URL">
+<node CREATED="1468281970602" ID="ID_1159945903" MODIFIED="1468281971474" TEXT="&lt;a href=&quot;html_images.asp&quot;&gt;HTML Images&lt;/a&gt;"/>
+<node CREATED="1468281972787" ID="ID_1028088731" MODIFIED="1468281990774" TEXT="without the https://www"/>
+</node>
+<node CREATED="1468282007058" ID="ID_1387361973" MODIFIED="1468282013958" TEXT="Colors">
+<node CREATED="1468282048891" ID="ID_778406907" MODIFIED="1468282049208" TEXT="When you move the mouse over a link, two things will normally happen:">
+<node CREATED="1468282066707" ID="ID_1634953939" MODIFIED="1468282066914" TEXT="The mouse arrow will turn into a little hand"/>
+<node CREATED="1468282074114" ID="ID_1467150079" MODIFIED="1468282074537" TEXT="The color of the link element will change"/>
+</node>
+<node CREATED="1468282051067" ID="ID_729404417" MODIFIED="1468282059544" TEXT="By default, a link will appear like this (in all browsers):">
+<node CREATED="1468282082363" ID="ID_1474957955" MODIFIED="1468282109685" TEXT="An unvisited link is underlined and blue"/>
+<node CREATED="1468282088499" ID="ID_1895422055" MODIFIED="1468282114277" TEXT="A visited link is underlined and purple"/>
+<node CREATED="1468282095067" ID="ID_5787995" MODIFIED="1468282117805" TEXT="An active link is underlined and red"/>
+</node>
+<node CREATED="1468282121507" ID="ID_1827750891" MODIFIED="1468282124217" TEXT="You can change the default colors, by using styles"/>
+</node>
+<node CREATED="1468282137483" ID="ID_1940681072" MODIFIED="1468282141256" TEXT="Target attribute">
+<node CREATED="1468282162275" ID="ID_1778835057" MODIFIED="1468282162545" TEXT="The target attribute specifies where to open the linked document."/>
+<node CREATED="1468282169139" ID="ID_1202944422" MODIFIED="1468282169313" TEXT="The target attribute can have one of the following values:">
+<node CREATED="1468282175211" ID="ID_726916584" MODIFIED="1468282175919" TEXT="_blank - Opens the linked document in a new window or tab"/>
+<node CREATED="1468282185443" ID="ID_1374246808" MODIFIED="1468282185849" TEXT="_self - Opens the linked document in the same window/tab as it was clicked (this is default)"/>
+<node CREATED="1468282193243" ID="ID_737276913" MODIFIED="1468282193564" TEXT="_parent - Opens the linked document in the parent frame"/>
+<node CREATED="1468282201747" ID="ID_1517486668" MODIFIED="1468282202617" TEXT="_top - Opens the linked document in the full body of the window"/>
+<node CREATED="1468282209570" ID="ID_1356199666" MODIFIED="1468282210785" TEXT="framename - Opens the linked document in a named frame"/>
+</node>
+<node COLOR="#990000" CREATED="1468282236075" ID="ID_1623235734" MODIFIED="1468282242299" TEXT="Tip: If your webpage is locked in a frame, you can use target=&quot;_top&quot; to break out of the frame"/>
+</node>
+<node CREATED="1468282278811" ID="ID_1089055882" MODIFIED="1468282283527" TEXT="Image links">
+<node CREATED="1468282291771" ID="ID_71979969" MODIFIED="1468282306802" TEXT="in  &lt;a&gt; tag there&apos;s an image"/>
+</node>
+<node CREATED="1468282366786" ID="ID_1564761742" MODIFIED="1468282369866" TEXT="Bookmarks">
+<node CREATED="1468282370202" ID="ID_1501910050" MODIFIED="1468282371329" TEXT="HTML bookmarks are used to allow readers to jump to specific parts of a Web page."/>
+<node CREATED="1468282380603" ID="ID_1325850191" MODIFIED="1468282380898" TEXT=" To make a bookmark, you must first create the bookmark, and then add a link to it."/>
+<node CREATED="1468282386611" ID="ID_1926484549" MODIFIED="1468282407121" TEXT="id and tha #id with href for it">
+<node CREATED="1468282445098" ID="ID_603457459" MODIFIED="1468282453375" TEXT="based on which page there it is"/>
+</node>
+</node>
 </node>
 </node>
 </node>
