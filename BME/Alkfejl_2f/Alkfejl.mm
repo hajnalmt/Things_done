db8e8984bf1e4f7579c4fc0e951b99202db08882
Mate Hajnal <bhumhajn@europe.ad.flextronics.com>
2016-09-06 12:35:52 +0200

    The semester has sarted

---
+++ BME/Alkfejl_2f/Alkfejl.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1473157950740" ID="ID_1979878129" MODIFIED="1473157981008" TEXT="Alkalmaz&#xe1;sfejleszt&#xe9;s">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</map>
---
+++ BME/Java_technologia_2f/javatech.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1473157894888" ID="ID_1754477906" MODIFIED="1473157904667" TEXT="Java technol&#xf3;gia">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</map>
