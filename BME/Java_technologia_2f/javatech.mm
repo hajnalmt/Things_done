1a3c8c84179279e641201ab60d466228423be5fe
Mate Hajnal <bhumhajn@europe.ad.flextronics.com>
2016-09-06 13:50:38 +0200

    Java technolgy first audiance

--- BME/Java_technologia_2f/javatech.mm
+++ BME/Java_technologia_2f/javatech.mm
@@ -2,5 +2,155 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1473157894888" ID="ID_1754477906" MODIFIED="1473157904667" TEXT="Java technol&#xf3;gia">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1473158212003" ID="ID_1237792070" MODIFIED="1473162563656" POSITION="right" TEXT="1.) A JAVA vil&#xe1;ga">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1473158230024" ID="ID_387345330" MODIFIED="1473158255695" TEXT="Hotspot">
+<node CREATED="1473158239850" ID="ID_872702783" MODIFIED="1473158327126" TEXT="&quot;A&quot; JVM, 7-es verzi&#xf3; &#xf3;ta ez a referencia implement&#xe1;ci&#xf3;"/>
+<node CREATED="1473158329066" ID="ID_1930806578" MODIFIED="1473158406209" TEXT="A SUN &#xe1;ltal ny&#xed;lt forr&#xe1;sk&#xf3;d&#xfa;v&#xe1; lett, jelenleg az Oracle fejleszti"/>
+</node>
+<node CREATED="1473158257206" ID="ID_691264968" MODIFIED="1473158273972" TEXT="Kell hozz&#xe1;  virtu&#xe1;lis g&#xe9;p">
+<node CREATED="1473158268110" ID="ID_1628971838" MODIFIED="1473158302886" TEXT="Jelenleg egy kicsit elavult nyelvnek mondj&#xe1;k"/>
+</node>
+<node CREATED="1473158305404" ID="ID_1885182598" MODIFIED="1473158365651" TEXT="Apache Harmony">
+<node CREATED="1473158366999" ID="ID_1958250345" MODIFIED="1473158398503" TEXT="Apache Software Foundation ny&#xed;lt forr&#xe1;sk&#xf3;d&#xfa; fejleszt&#xe9;se"/>
+</node>
+<node CREATED="1473158409257" ID="ID_128820017" MODIFIED="1473158430182" TEXT="JRockit">
+<node CREATED="1473158436393" ID="ID_430802957" MODIFIED="1473158440142" TEXT="Android"/>
+</node>
+<node CREATED="1473158465437" ID="ID_665347647" MODIFIED="1473158470629" TEXT="A t&#xe1;rgy t&#xe9;m&#xe1;ja">
+<node CREATED="1473158472829" ID="ID_237667036" MODIFIED="1473158485088" TEXT="Jelenleg as verzi&#xf3; fut"/>
+<node CREATED="1473158490011" ID="ID_952106602" MODIFIED="1473158492691" TEXT="M&#xe9;rf&#xf6;ldk&#xf6;vek"/>
+</node>
+<node CREATED="1473158498596" ID="ID_1603936628" MODIFIED="1473158503650" TEXT="Egy&#xe9;b nyelvek a JVM felett">
+<node CREATED="1473158505472" ID="ID_515915695" MODIFIED="1473158517668" TEXT="JVM-re portolt nyelvek">
+<node CREATED="1473158518493" ID="ID_1288838881" MODIFIED="1473158524791" TEXT="JRuby"/>
+<node CREATED="1473158526619" ID="ID_1079121674" MODIFIED="1473158529385" TEXT="Jython"/>
+<node CREATED="1473158530706" ID="ID_305305116" MODIFIED="1473158537471" TEXT="Free Pascal"/>
+</node>
+<node CREATED="1473158539360" ID="ID_757377512" MODIFIED="1473158547367" TEXT="JVM-hez tervezettt nyelvek">
+<node CREATED="1473158548332" ID="ID_390472526" MODIFIED="1473158554038" TEXT="Clojure">
+<node CREATED="1473158657561" ID="ID_868603371" MODIFIED="1473158659881" TEXT=" funkc nyelv"/>
+</node>
+<node CREATED="1473158557181" ID="ID_354041475" MODIFIED="1473158559433" TEXT="Scala">
+<node CREATED="1473158661486" ID="ID_548027851" MODIFIED="1473158666497" TEXT="Olyan mint a JAVA++"/>
+<node CREATED="1473158667739" ID="ID_1235949707" MODIFIED="1473158690242" TEXT="Sokkal fejlesztettebb eszk&#xf6;z&#xf6;ket ad a programoz&#xf3;k kez&#xe9;be"/>
+</node>
+<node CREATED="1473158560999" ID="ID_362127379" MODIFIED="1473158565887" TEXT="Groovy"/>
+</node>
+</node>
+<node CREATED="1473158784385" ID="ID_1084903181" MODIFIED="1473158809412" TEXT="Els&#x151;sorban parancssori eszk&#xf6;z&#xf6;k, pl.:">
+<node CREATED="1473158809937" ID="ID_613238859" MODIFIED="1473158813583" TEXT="java">
+<node CREATED="1473158813928" ID="ID_1652428118" MODIFIED="1473158825124" TEXT="bet&#xf6;lt&#x151; &#xe9;s futtat&#xf3; k&#xf6;rnyezet"/>
+</node>
+<node CREATED="1473158826730" ID="ID_1814321702" MODIFIED="1473158830126" TEXT="javac">
+<node CREATED="1473158830454" ID="ID_1703648510" MODIFIED="1473158834950" TEXT="java ford&#xed;t&#xf3;"/>
+</node>
+<node CREATED="1473158836480" ID="ID_1367433868" MODIFIED="1473158841482" TEXT="javadoc">
+<node CREATED="1473158842080" ID="ID_1666898494" MODIFIED="1473158849111" TEXT="dokument&#xe1;ci&#xf3; gener&#xe1;tor"/>
+</node>
+<node CREATED="1473158850578" ID="ID_1647947552" MODIFIED="1473158852514" TEXT="jar">
+<node CREATED="1473158854664" ID="ID_355322344" MODIFIED="1473158857656" TEXT="Java alkalmaz&#xe1;sok"/>
+</node>
+<node CREATED="1473158859644" ID="ID_117258416" MODIFIED="1473158866454" TEXT="VisualVM">
+<node CREATED="1473158867314" ID="ID_423453752" MODIFIED="1473158876169" TEXT="profiler (teljes&#xed;tm&#xe9;nyelemz&#x151;)"/>
+<node CREATED="1473158877805" ID="ID_453054365" MODIFIED="1473158895139" TEXT="Pontosan l&#xe1;tni, hogy h&#xe1;ny sz&#xe1;l futni meg mennyire kihaszn&#xe1;lt a mem&#xf3;ria"/>
+</node>
+</node>
+<node CREATED="1473158900413" FOLDED="true" ID="ID_1484156214" MODIFIED="1473162227088" TEXT="Nyelvi elemek">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1473158919906" ID="ID_1562997136" MODIFIED="1473158942362" TEXT="A defin&#xed;ci&#xf3;ja">
+<node CREATED="1473158944394" ID="ID_84410153" MODIFIED="1473158951020" TEXT="Language specification"/>
+</node>
+<node CREATED="1473158984115" ID="ID_282310233" MODIFIED="1473158990966" TEXT="karakterk&#xf3;dol&#xe1;s">
+<node CREATED="1473158991327" ID="ID_1868082675" MODIFIED="1473158997655" TEXT="UTF8 a leggyakoribb"/>
+</node>
+<node CREATED="1473159012407" ID="ID_1615280913" MODIFIED="1473159024883" TEXT="C&#xe9;l: A forr&#xe1;sk&#xf3;dok is hordozhat&#xf3;ak legyenek."/>
+<node CREATED="1473159026538" ID="ID_704241393" MODIFIED="1473159049164" TEXT="Megold&#xe1;s: A forr&#xe1;sk&#xf3;dik Unicode karakterekb&#x151;l &#xe1;llnak.">
+<node CREATED="1473159183731" ID="ID_1690507948" MODIFIED="1473159205320" TEXT="-&gt; a forr&#xe1;sk&#xf3;dban el&#x151;fordulnak unicode escape szekvenci&#xe1;k"/>
+<node CREATED="1473159395662" ID="ID_831699038" MODIFIED="1473159407789" TEXT="Az unicode escapek form&#xe1;tuma a k&#xf6;vetkez&#x151;">
+<node CREATED="1473159409233" ID="ID_464902736" MODIFIED="1473159416438" TEXT="\u1234"/>
+</node>
+<node CREATED="1473159418499" ID="ID_105916972" MODIFIED="1473159429946" TEXT="ahol az u ut&#xe1;n n&#xe9;gy sz&#xe1;m van az escape karakter"/>
+</node>
+<node CREATED="1473159437552" ID="ID_1018927618" MODIFIED="1473159441067" TEXT="Megyjegyz&#xe9;sek">
+<node CREATED="1473159442016" ID="ID_893530741" MODIFIED="1473159445430" TEXT="mint C-ben"/>
+</node>
+<node CREATED="1473159449630" ID="ID_181484144" MODIFIED="1473159479927" TEXT="N&#xe9;v, azonos&#xed;t&#xf3; (identifier)">
+<node CREATED="1473159497186" ID="ID_1222766866" MODIFIED="1473159524513" TEXT="a Character.is.JavaidentifierStart met&#xf3;dusnak true-val kell visszat&#xe9;rnie"/>
+<node CREATED="1473159531477" ID="ID_1699657548" MODIFIED="1473159536092" TEXT="nem lehet kulcssz&#xf3;">
+<node CREATED="1473159607269" ID="ID_581932002" MODIFIED="1473159613814" TEXT="van egy kulcssz&#xf3; lista">
+<node CREATED="1473159622172" ID="ID_387319893" MODIFIED="1473159673256" TEXT="a const &#xe9;s a goto m&#xe1;r nem azok"/>
+<node CREATED="1473159674592" ID="ID_1148309380" MODIFIED="1473159713974" TEXT="az assert meg az enum m&#xe1;r azok "/>
+</node>
+</node>
+<node CREATED="1473159548334" ID="ID_482937125" MODIFIED="1473159566186" TEXT="VIGY&#xc1;ZAT mert azonosnak t&#x171;n&#xf6; karakterek lehet k&#xfc;l&#xf6;nb&#xf6;z&#x151;ek">
+<node CREATED="1473159568676" ID="ID_189142255" MODIFIED="1473159576774" TEXT="text editorban &#xe9;rdemes ezt megn&#xe9;zni"/>
+</node>
+<node CREATED="1473159716548" ID="ID_682467078" MODIFIED="1473159719005" TEXT="liter&#xe1;lok"/>
+<node CREATED="1473159835469" ID="ID_1835450392" MODIFIED="1473159840025" TEXT="t&#xed;pus m&#xe9;retek">
+<node CREATED="1473159890338" ID="ID_919277276" MODIFIED="1473159892708" TEXT="meghat&#xe1;rozva"/>
+<node CREATED="1473159893748" ID="ID_318191004" MODIFIED="1473160001398" TEXT="h&#xe1;rom plusz t&#xed;pus a lebeg&#x151;pontoz&#xe1;shoz">
+<node CREATED="1473159905475" ID="ID_639422951" MODIFIED="1473159920935" TEXT="NaN (not a number)"/>
+<node CREATED="1473159922249" ID="ID_366888912" MODIFIED="1473159936514" TEXT="Positive negativ infinity"/>
+</node>
+<node COLOR="#990000" CREATED="1473160019126" ID="ID_1834749841" MODIFIED="1473160103298" TEXT="Minden NaN &#xe9;rt&#xe9;ken v&#xe9;gzett m&#x171;velet eredm&#xe9;nye NaN"/>
+<node COLOR="#990000" CREATED="1473160039668" ID="ID_1796942910" MODIFIED="1473160102606" TEXT="Minden NaN &#xe9;rt&#xe9;kben v&#xe9;gzett &#xf6;sszehasonl&#xed;t&#xf3; m&#x171;velet &#xe9;rt&#xe9;ke NaN! (M&#xe9;g akkor is ha a m&#xe1;sik &#xe9;rt&#xe9;k is NaN)"/>
+</node>
+<node CREATED="1473160109776" ID="ID_1336386290" MODIFIED="1473160131166" TEXT="A primit&#xed;v t&#xed;pusok nem objektumok (ellent&#xe9;tben p&#xe9;ld&#xe1;ul a c#-al)">
+<node CREATED="1473160137716" ID="ID_770029186" MODIFIED="1473160154265" TEXT="Nem lehet r&#xe1;juk referenci&#xe1;val hivatkozni"/>
+</node>
+<node CREATED="1473160155872" ID="ID_1177575666" MODIFIED="1473160161913" TEXT="Burkol&#xf3; oszt&#xe1;ly">
+<node CREATED="1473160168180" ID="ID_902993840" MODIFIED="1473160177699" TEXT="Objektumokk&#xe9;nt kezelhet&#x151;"/>
+<node CREATED="1473160181162" ID="ID_917170607" MODIFIED="1473160205344" TEXT=" A neve &#xe1;ltal&#xe1;ban a primit&#xed;v tipus nev&#xe9;nek nagy kezd&#x151;bet&#x171;s v&#xe1;ltozata"/>
+<node CREATED="1473160208016" ID="ID_326832824" MODIFIED="1473160211809" TEXT="AUTOBOXING">
+<node CREATED="1473160213024" ID="ID_423702795" MODIFIED="1473160225638" TEXT="M&#xe1;r megcsin&#xe1;lja helyett&#xfc;nk 1.5 &#xf3;ta a JAVA"/>
+</node>
+</node>
+</node>
+<node CREATED="1473159732257" ID="ID_213983138" MODIFIED="1473160903154" TEXT="T&#xed;puskonverzi&#xf3;k 1.">
+<node CREATED="1473160903596" ID="ID_1629890015" MODIFIED="1473160914611" TEXT="K&#xe9;t fajta van">
+<node CREATED="1473160915102" ID="ID_696700583" MODIFIED="1473160917131" TEXT="implicit">
+<node CREATED="1473160927351" ID="ID_1540755452" MODIFIED="1473160930034" TEXT="automatikus"/>
+</node>
+<node CREATED="1473160918876" ID="ID_1941807136" MODIFIED="1473160921409" TEXT="explicit">
+<node CREATED="1473160932309" ID="ID_342098044" MODIFIED="1473160936895" TEXT="nem automatikus"/>
+</node>
+<node CREATED="1473160947985" ID="ID_1574526281" MODIFIED="1473160968692" TEXT="a b&#x151;v&#xed;t&#x151; ir&#xe1;nyban m&#x171;k&#xf6;dik az automatikus csak"/>
+</node>
+<node CREATED="1473160980683" ID="ID_982777991" MODIFIED="1473161003315" TEXT="K&#xe9;toperandus&#xfa; m&#x171;veletn&#xe9;l az operandusok t&#xed;pusa d&#xf6;nti el az eredm&#xe9;ny tipus&#xe1;t"/>
+<node CREATED="1473161011393" ID="ID_173978632" MODIFIED="1473161055029" TEXT="Az eredm&#xe9;ny t&#xed;pusa mindig a k&#xe9;r operandus k&#xf6;z&#xfc;l a b&#x151;vebb tipus&#xfa; operandus k&#xf6;z&#xfc;l a b&#x151;vebb tipus&#xfa; operandus tipus&#xe1;val egyezik meg"/>
+<node CREATED="1473161213366" ID="ID_906706150" MODIFIED="1473161240743" TEXT="Fontos az objektum, burkol&#xf3;t&#xed;pus &#xe9;s a sima t&#xed;pus k&#xf6;zti k&#xfc;l&#xf6;nbs&#xe9;g"/>
+<node CREATED="1473161310048" ID="ID_283305885" MODIFIED="1473161320156" TEXT="Nincs el&#x151;re deklar&#xe1;ci&#xf3;">
+<node CREATED="1473161321093" ID="ID_1337830244" MODIFIED="1473161332359" TEXT="nem &#xfa;gy, mint C-ben"/>
+</node>
+</node>
+<node CREATED="1473161158320" ID="ID_1117776614" MODIFIED="1473161170838" TEXT="A JAVA-ban csak met&#xf3;dusok vannak."/>
+<node CREATED="1473161512330" ID="ID_1853510833" MODIFIED="1473161515870" TEXT="Oper&#xe1;torok">
+<node CREATED="1473161603427" ID="ID_498177348" MODIFIED="1473161635621" TEXT="Nincs sizeof vagy -&gt; vagy &amp; c&#xed;m vagy * pointer indirekci&#xf3; semmi hasonl&#xf3;">
+<node CREATED="1473161637020" ID="ID_1102548809" MODIFIED="1473161642509" TEXT="Egy oper&#xe1;tor van a ."/>
+</node>
+<node CREATED="1473161652772" ID="ID_690404646" MODIFIED="1473161661355" TEXT="F&#xfc;ggv&#xe9;ny ki&#xe9;rt&#xe9;kel&#xe9;se balr&#xf3;l jobbra">
+<node CREATED="1473161669253" ID="ID_248950206" MODIFIED="1473161678816" TEXT="C-ben ez tetsz&#x151;leges sorrendben t&#xf6;rt&#xe9;nik, az&#xe9;rt &#xe9;rdekes"/>
+</node>
+<node CREATED="1473161662528" ID="ID_1770825738" MODIFIED="1473161721803" TEXT="Nincs r&#xf6;vidrez&#xe1;rt ki&#xe9;rt&#xe9;kel&#xe9;s csak AND &#xe9;s OR eset&#xe9;ben ">
+<node CREATED="1473161730535" ID="ID_1753482521" MODIFIED="1473161770526" TEXT="C-ben ugye ez mindig meg van pl 0*f()-n&#xe9;l nem biztos, hogy f ki&#xe9;rt&#xe9;kel&#x151;dik"/>
+</node>
+</node>
+</node>
+<node CREATED="1473162222936" ID="ID_112336008" MODIFIED="1473162239682" TEXT="el&#xe1;gaz&#xe1;sok, ciklusszervez&#xe9;s">
+<node CREATED="1473162240524" ID="ID_1792578916" MODIFIED="1473162248406" TEXT="teljesen ugyan&#xfa;gy mint C-be"/>
+<node CREATED="1473162275419" ID="ID_1197693801" MODIFIED="1473162293145" TEXT="a for-n&#xe1;l lehet deklar&#xe1;lni az el&#x151;felt&#xe9;telben"/>
+<node CREATED="1473162342851" ID="ID_19153401" MODIFIED="1473162358775" TEXT="lehet &#xfa;gy is defini&#xe1;kni a for ciklust, hogy">
+<node CREATED="1473162359170" ID="ID_1824876561" MODIFIED="1473162375847" TEXT="for (t&#xed;pusazonos&#xed;t&#xf3; : kifejez&#xe9;s) utas&#xed;t&#xe1;s"/>
+</node>
+</node>
+<node CREATED="1473162566271" ID="ID_452374953" MODIFIED="1473162598612" TEXT="Ami meglep&#x151; a nyelvben, hogy UTF-16-os karakterk&#xf3;dol&#xe1;st tarrtalmaz, erre figyelni kell mindig."/>
+</node>
+<node CREATED="1473159756829" ID="ID_1599757443" MODIFIED="1473159781128" POSITION="left" TEXT="K&#xf6;vetelm&#xe9;nyek">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1473159761835" ID="ID_430348163" MODIFIED="1473159781620" POSITION="left" TEXT="NIncs jelenl&#xe9;t ellen&#x151;rz&#xe9;s, egy HF van + vizsga">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
 </node>
 </map>
