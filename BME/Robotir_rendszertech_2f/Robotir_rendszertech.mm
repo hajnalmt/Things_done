47e5e5ec67da399f3782f3383444fa6c32506bb8
Mate Hajnal <bhumhajn@europe.ad.flextronics.com>
2016-09-07 12:36:49 +0200

    Robotir audiance mindmap extended

--- BME/Robotir_rendszertech_2f/Robotir_rendszertech.mm
+++ BME/Robotir_rendszertech_2f/Robotir_rendszertech.mm
@@ -3,9 +3,58 @@
 <node CREATED="1473230417893" ID="ID_553062540" MODIFIED="1473230438225" TEXT="Robotir&#xe1;ny&#xed;t&#xe1;s rendszertechnik&#xe1;ja">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1473230601284" ID="ID_1063054043" MODIFIED="1473230601284" POSITION="left" TEXT=""/>
-<node CREATED="1473230617078" ID="ID_1440859158" MODIFIED="1473230628278" POSITION="right" TEXT="1. el&#x151;ad&#xe1;s">
+<node CREATED="1473230617078" FOLDED="true" ID="ID_1440859158" MODIFIED="1473235387233" POSITION="right" TEXT="1. el&#x151;ad&#xe1;s">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1473230589322" ID="ID_639007719" MODIFIED="1473230625694" TEXT="Mintatanterv"/>
+<node CREATED="1473232381076" ID="ID_1680990312" MODIFIED="1473232392811" TEXT="K&#xf6;vetelem&#xe9;nyek"/>
+<node CREATED="1473232395191" FOLDED="true" ID="ID_1139774497" MODIFIED="1473234844129" TEXT="1.1 A robot, mint ir&#xe1;ny&#xed;tand&#xf3; folyamat">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1473232413915" ID="ID_1963809036" MODIFIED="1473232471939" TEXT="A robotokhoz vannak sorolva az inteligens is"/>
+<node COLOR="#338800" CREATED="1473232463203" ID="ID_633966145" MODIFIED="1473232539395" TEXT="A robot egy olyan mesters&#xe9;gesen el&#x151;&#xe1;ll&#xed;tott (elektro)mechanikai szerkezet, amely el&#x151;&#xed;rhat&#xf3; p&#xe1;ly&#xe1;n mozog, a p&#xe1;lya ment&#xe9;n vagy annak meghat&#xe1;rozott pontjaiban el&#x151;&#xed;rhat&#xf3; feladatokat l&#xe1;t el."/>
+<node CREATED="1473232541080" ID="ID_310531004" MODIFIED="1473232614854" TEXT="Az emberi k&#xe9;z alkalmatlan m&#xe1;r ilyen szint&#x171; feladatok elv&#xe9;gz&#xe9;s&#xe9;re"/>
+<node CREATED="1473232797369" ID="ID_1271671773" MODIFIED="1473232822401" TEXT="Kempelen Farkas sakkoz&#xf3; automat&#xe1;ja (1769)"/>
+<node CREATED="1473232902833" ID="ID_1316679062" MODIFIED="1473232918155" TEXT="Az els&#x151; auton&#xf3;m szerel&#x151; robot 1960-ban">
+<node CREATED="1473232919132" ID="ID_1501181796" MODIFIED="1473232928107" TEXT="Unimates - General Motors"/>
+</node>
+<node CREATED="1473233086384" ID="ID_1657771447" MODIFIED="1473233101747" TEXT="Csukl&#xf3;t&#xed;pusok">
+<node CREATED="1473233242564" ID="ID_1196084000" MODIFIED="1473233248972" TEXT="A mozg&#xe1;si lehet&#x151;s&#xe9;gek">
+<node CREATED="1473233249889" ID="ID_772607050" MODIFIED="1473233252506" TEXT="Elfordul&#xe1;s">
+<node CREATED="1473233292874" ID="ID_1356417407" MODIFIED="1473233304492" TEXT="R -rotatetel van jel&#xf6;lve az &#xe1;br&#xe1;n"/>
+</node>
+<node CREATED="1473233253622" ID="ID_1598148775" MODIFIED="1473233280310" TEXT="Transzl&#xe1;ci&#xf3;">
+<node CREATED="1473233306825" ID="ID_1856167585" MODIFIED="1473233322169" TEXT="T-vel van jel&#xf6;lve az &#xe1;br&#xe1;n"/>
+</node>
+<node CREATED="1473233281968" ID="ID_631870375" MODIFIED="1473233291053" TEXT="Minden csukl&#xf3; egy szabads&#xe1;gfok">
+<node CREATED="1473233634500" ID="ID_633300496" MODIFIED="1473233639160" TEXT="6 &#xe1;ltal&#xe1;ban el&#xe9;g"/>
+</node>
+</node>
+</node>
+<node CREATED="1473233618820" ID="ID_1986907315" MODIFIED="1473233630852" TEXT="Ny&#xed;lt l&#xe1;nc&#xfa; robotkar">
+<node CREATED="1473233676551" ID="ID_1451715256" MODIFIED="1473233686394" TEXT="T&#xf6;bb megold&#xe1;s is lehet egy probl&#xe9;m&#xe1;ra"/>
+</node>
+<node CREATED="1473233862600" ID="ID_164949033" MODIFIED="1473233875752" TEXT="Descartes robotok">
+<node CREATED="1473233876670" ID="ID_573270106" MODIFIED="1473233905151" TEXT="h&#xe1;rom transzl&#xe1;ci&#xf3;s mozg&#xe1;si lehet&#x151;s&#xe9;ggel - h&#xe1;rom szabads&#xe1;gi fok"/>
+<node CREATED="1473233906373" ID="ID_1154814170" MODIFIED="1473233917308" TEXT="A pick and place robotok"/>
+<node CREATED="1473233919086" ID="ID_1867389134" MODIFIED="1473233931215" TEXT="vagy rakt&#xe1;rpakol&#xf3; robotok is ilyenek"/>
+</node>
+<node CREATED="1473234061831" ID="ID_1741583868" MODIFIED="1473234086087" TEXT="Hengerkoordin&#xe1;t&#xe1;s"/>
+<node CREATED="1473234066890" ID="ID_796297099" MODIFIED="1473234081732" TEXT="Tagolt (csukl&#xf3;s)"/>
+<node CREATED="1473234202942" ID="ID_911620185" MODIFIED="1473234224337" TEXT="A Mitsubishi RV-3SDB &quot;emberi terstr&#xe9;szei&quot;">
+<node CREATED="1473234225378" ID="ID_586428654" MODIFIED="1473234242143" TEXT="Humanoid robot - Az emberi test mozg&#xe1;s&#xe1;t  tudja szimul&#xe1;lni."/>
+</node>
+</node>
+<node CREATED="1473234850921" FOLDED="true" ID="ID_1605137827" MODIFIED="1473235384501" TEXT="1.2 Fejsz&#xe1;mol&#xe1;s">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1473234868595" ID="ID_1552127032" MODIFIED="1473234879955" TEXT="3 mikrokontroller eredm&#xe9;nyei"/>
+<node CREATED="1473235001875" ID="ID_1757971762" MODIFIED="1473235027592" TEXT="Digit&#xe1;lis jel feldolgoz&#xe1;sa alapm&#x171;veletekkel"/>
+<node CREATED="1473235035946" ID="ID_1489998763" MODIFIED="1473235039649" TEXT="specifik&#xe1;c&#xf3;">
+<node CREATED="1473235040616" ID="ID_1011567433" MODIFIED="1473235063272" TEXT="100 us cimlusid&#x151;"/>
+<node CREATED="1473235064625" ID="ID_1201181434" MODIFIED="1473235088252" TEXT="15-20 m&#x171;velet (&#xf6;sszead&#xe1;s, logikai &#xc9;S/VAGY)"/>
+<node CREATED="1473235089795" ID="ID_1136690637" MODIFIED="1473235093816" TEXT="32 db bemenet"/>
+</node>
+<node CREATED="1473235196756" ID="ID_920919690" MODIFIED="1473235217325" TEXT="Hajt&#xe1;sszab&#xe1;lyoz&#xe1;s PID algoritmussal"/>
+<node CREATED="1473235339690" ID="ID_1777171944" MODIFIED="1473235379422" TEXT="P&#xe1;lyatervez&#xe9;s: 4*4 m&#xe1;trixok szorz&#xe1;sa (10 db)"/>
+</node>
 </node>
 </node>
 </map>
