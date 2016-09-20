c3b537f5374d92fa09462625d243664f39d12f78
Hajnal Mate <hajnalmt@gmail.com>
2016-09-20 10:25:24 +0200

    Keszulektervezes mindmap added

---
+++ BME/Keszulektervezes_2f/Keszulektervezes.mm
@@ -0,0 +1,7 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1474359769904" ID="ID_163598882" MODIFIED="1474359877689" TEXT="K&#xe9;sz&#xfc;l&#xe9;ktervez&#xe9;s">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1474359879280" ID="ID_1662785594" MODIFIED="1474359890413" POSITION="right" TEXT="Diszkr&#xe9;t l&#xe9;gr&#xe9;ses fojt&#xf3;"/>
+</node>
+</map>
---
+++ Elte/Objektumelv_alkfejl_2f/Objekt_alkfejl.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1473848071220" ID="ID_1756919846" MODIFIED="1473848166985" TEXT="Objektumelv&#x171; alkalmaz&#xe1;sfejleszt&#xe9;s">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</map>
